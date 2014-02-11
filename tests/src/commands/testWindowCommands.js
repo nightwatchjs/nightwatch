b950b59678cf7eb87a2e46abe2e87022a74e6ac4
andrei <andrei.rusu@beatfactor.net>
2014-02-11 10:03:57 +0100

    added closeWindow and switchWindow commands and an example test for performing facebook login authentication on digg.com website

---
+++ tests/src/commands/testWindowCommands.js
@@ -0,0 +1,27 @@
+module.exports = {
+  setUp: function (callback) {
+    this.client = require('../../nightwatch.js').init();
+    callback();
+  },
+  
+  testCloseWindow : function(test) {
+    var client = this.client;
+    var command = client.closeWindow(function callback() {
+      test.done();  
+    });
+  },
+  
+  testSwitchWindow : function(test) {
+    var client = this.client;
+    var command = client.closeWindow(function callback() {
+      test.done();  
+    });
+    
+  },
+               
+  tearDown : function(callback) {
+    this.client = null;
+    // clean up
+    callback();
+  }
+}
