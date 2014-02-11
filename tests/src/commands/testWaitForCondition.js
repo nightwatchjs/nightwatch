6630c924146615313fad802269de24d36bbceb64
Steve Calvert <scalvert@mobify.com>
2014-02-11 11:03:55 -0800

    Adding custom commands. Added tests for waitForCondition - needs JSON wire API mock completed to work

---
+++ tests/src/commands/testWaitForCondition.js
@@ -0,0 +1,31 @@
+var MockServer  = require('mockserver');
+    
+module.exports = {
+  setUp: function (callback) {
+    this.client = require('../../nightwatch.js').init();
+    
+    callback();
+  },
+  
+  testSuccess : function(test) {
+    debugger;
+    this.client.waitForCondition('return true', 100, function callback(result) {
+      test.ok(result !== false);
+      test.done();
+    });
+  },
+  
+  testFailure : function(test) {
+    debugger;
+    this.client.waitForCondition('return false', 600, function callback(result) {
+      test.equal(result, false);
+      test.done();
+    });
+  },
+              
+  tearDown : function(callback) {
+    this.client = null;
+    // clean up
+    callback();
+  }
+}
