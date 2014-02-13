1d52ef8b40a23628ea6a387718169478aa7e365d
Keith Bingman <keith@mobify.com>
2014-02-12 16:05:10 -0800

    removed waitForAjaxCompleted and waitForPageToBeMobified in favour of waitForCondition.

--- tests/src/commands/testWaitForCondition.js
+++ tests/src/commands/testWaitForCondition.js
@@ -8,15 +8,29 @@ module.exports = {
   },
 
   testSuccess : function(test) {
-    this.client.waitForCondition('return true', 100, function callback(result) {
+    this.client.waitForCondition('return true', 100, 0, function callback(result) {
       test.ok(result !== false);
       test.done();
     });
   },
 
   testFailure : function(test) {
-    this.client.waitForCondition('return false', 600, function callback(result) {
+    this.client.waitForCondition('return false', 600, 0, function callback(result) {
       test.equal(result, false);
+      test.done();
+    });
+  },
+
+  testNoTimeout : function(test) {
+    this.client.waitForCondition('return true', 100, function callback(result) {
+      test.ok(result !== false);
+      test.done();
+    });
+  },
+
+  testWaitForPageToBeMobified : function(test) {
+    this.client.waitForCondition('return Mobify.evaluatedData', 1000, 1000, function callback(result) {
+      test.ok(result !== false);
       test.done();
     });
   },
