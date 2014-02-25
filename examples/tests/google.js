25831b4a12c211a4310f1e4c5edfb4cc1a03b77e
andrei <andrei.rusu@beatfactor.net>
2014-02-25 22:52:03 +0100

    more refactoring

--- examples/tests/google.js
+++ examples/tests/google.js
@@ -1,19 +1,19 @@
 /**
  * Sample automated test scenario for Nightwatch.js
- * 
+ *
  * > it navigates to google.com and searches for nightwatch,
- *   verifying if the term 'The Night Watch' exists in the search results  
+ *   verifying if the term 'The Night Watch' exists in the search results
  */
 
 module.exports = {
   setUp : function(c) {
-  	console.log('Setting up...');   
+  	console.log('Setting up...');
   },
-  
+
   tearDown : function() {
     console.log('Closing down...');
   },
-  
+
   'step one' : function (client) {
     client
       .url('http://google.com')
@@ -23,18 +23,20 @@ module.exports = {
       })
       .assert.title('Google')
       .url(function(result) {
-        this.assert.ok(result.value.indexOf('google.nl') !== -1, 'Google url is ok');    
+        this.assert.ok(result.value.indexOf('google.nl') !== -1, 'Google url is ok');
       })
       .assert.visible('input[type=text]')
       .setValue('input[type=text]', 'nightwatch')
       .waitForElementVisible('button[name=btnG]', 1000);
   },
-  
+
   'step two' : function (client) {
     client
       .click('button[name=btnG]')
       .pause(1000)
       .assert.containsText('#main', 'The Night Watch')
-      .end();
+
   }
+
+
 }
