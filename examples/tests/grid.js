f970243ab551325942a6eb510f88601355c60eda
andrei <andrei.rusu@beatfactor.net>
2014-02-28 17:02:18 +0100

    removed broken examples

--- examples/tests/grid.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'should simply open and close': function (browser) {
-        browser
-          .url('http://github.com/beatfactor/nightwatch')
-          .pause(1000)
-          .end();
-    }
- };
