dc4c65817e0c4400ca40fc1647789ed8ef46d069
Joseph Jung <jungyue@gmail.com>
2014-03-27 11:01:10 -0500

    add blacklist support

--- runner/run.js
+++ runner/run.js
@@ -23,12 +23,14 @@ module.exports = new (function() {
   };
 
   function runModule(module, opts, moduleName, callback, finishCallback) {
+
     var client;
+    
     try {
       client = Nightwatch.client(opts);
     } catch (err) {
       console.log(err.stack);
-      finishCallback(err, false);
+      finishCallback(err);
       return;
     }
     var keys   = Object.keys(module);
@@ -155,16 +157,6 @@ module.exports = new (function() {
     }
   }
 
-  function processExitListener() {
-    process.on('exit', function(code) {
-      if (globalResults.errors > 0 || globalResults.failed > 0) {
-        process.exit(1);
-      } else {
-        process.exit(code);
-      }
-    });
-  }
-
   function wrapTest(setUp, tearDown, fn, context, onComplete, client) {
     return function (c) {
       context.client = c;
@@ -189,6 +181,7 @@ module.exports = new (function() {
       return cb(null, paths);
     }
 
+   
     paths.forEach(function(p) {
       walk(p, function(err, list) {
         if (err) {
@@ -254,41 +247,12 @@ module.exports = new (function() {
     });
   }
 
-  /**
-   * Get any subfolders relative to the base module path so that we can save the report files into their corresponding subfolders
-   *
-   * @param {string} modulePath
-   * @param {object} aditional_opts
-   * @returns {string}
-   */
-  function getPathDiff(modulePath, aditional_opts) {
-    var pathParts = modulePath.split(path.sep);
-    pathParts.pop();
-
-    var moduleFolder = pathParts.join(path.sep);
-    var diffInFolder = '', srcFolder;
-
-    if (Array.isArray(aditional_opts.src_folders)) {
-      for (var i = 0; i < aditional_opts.src_folders.length; i++) {
-        srcFolder = path.resolve(aditional_opts.src_folders[i]);
-        if (moduleFolder.indexOf(srcFolder) === 0) {
-          diffInFolder = moduleFolder.substring(srcFolder.length);
-          break;
-        }
-      }
-    } else if (typeof aditional_opts.src_folders == 'string') {
-      if (moduleFolder.indexOf(aditional_opts.src_folders) === 0) {
-        diffInFolder = moduleFolder.substring(aditional_opts.src_folders.length);
-      }
-    }
-    return diffInFolder;
-  }
-
   this.run = function runner(files, opts, aditional_opts, finishCallback) {
     var start = new Date().getTime();
     var modules = {};
     var curModule;
     var paths;
+    
 
     finishCallback = finishCallback || function() {};
 
@@ -299,6 +263,7 @@ module.exports = new (function() {
         return path.join(process.cwd(), p);
       });
     }
+
 
     if (paths.length === 0) {
       throw new Error('No tests to run.');
@@ -316,13 +281,37 @@ module.exports = new (function() {
       try {
         module = require(modulePath);
       } catch (err) {
-        finishCallback(err, false);
+        finishCallback(err);
         throw err;
       }
 
       var moduleName = modulePath.split(path.sep).pop();
       globalResults.modules[moduleName] = [];
       console.log('\n' + Logger.colors.cyan('[ ' + moduleName + ' module ]'));
+
+
+
+      function isInBlackList() {
+        
+        var blacklist = opts.blacklist,
+          testFolder = paths[0].match(/.+[\\\/]([^\\\/]+)$/)[1],
+          trimRegEx = new RegExp(testFolder + "[\\/\\\\](.+$)"),
+          trimmedModulePath = modulePath.match(trimRegEx)[1].replace(/\\/g, "/");
+
+        if (typeof blacklist !== "undefined") {
+          
+          if (typeof blacklist === "string" && blacklist === "all") {
+            return true;
+          }
+
+          if (typeof blacklist.files !== "undefined" && blacklist.files.indexOf(trimmedModulePath) !== -1) return true; 
+          if (typeof blacklist.files !== "undefined" && (blacklist.files.indexOf(trimmedModulePath + ".js")) !== -1) return true; 
+          if (typeof blacklist.directories !== "undefined" && blacklist.directories.indexOf(path.dirname(trimmedModulePath)) !== -1) return true;
+        }
+        return false;
+      }
+
+      module.disabled = isInBlackList();
 
       runModule(module, opts, moduleName, function(err, testresults, modulekeys) {
         if (typeof testresults == 'object') {
@@ -336,6 +325,7 @@ module.exports = new (function() {
 
         if (fullpaths.length) {
           setTimeout(function() {
+            
             runTestModule(err, fullpaths);
           }, 0);
         } else {
@@ -343,17 +333,15 @@ module.exports = new (function() {
             printResults(globalResults, modulekeys);
           }
 
-          var diffInFolder = getPathDiff(modulePath, aditional_opts);
-          var output = path.join(aditional_opts.output_folder, diffInFolder);
-          var success = globalResults.failed === 0 && globalResults.errors === 0;
+          var output = aditional_opts.output_folder;
           if (output === false) {
-            finishCallback(null, success);
+            finishCallback(null);
           } else {
             mkpath(output, function(err) {
               if (err) {
                 console.log(Logger.colors.yellow('Output folder doesn\'t exist and cannot be created.'));
                 console.log(err.stack);
-                finishCallback(null, success);
+                finishCallback(null);
                 return;
               }
 
@@ -362,15 +350,16 @@ module.exports = new (function() {
                   console.log(Logger.colors.yellow('Warning: Failed to save report file to folder: ' + output));
                   console.log(err.stack);
                 }
-                finishCallback(null, success);
+                finishCallback(null);
               });
             });
           }
         }
       }, finishCallback);
-    }, opts);
 
-    processExitListener();
+
+
+    }, opts);
   };
 })();
 
