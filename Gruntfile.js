e341ffdb6e73789086ae4d56ff006824a2661161
David Linse <davidlinse@gmail.com>
2014-06-09 23:07:42 +0200

    chore: remove options for unused uglify-task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,16 +5,6 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
     jshint: {
       options: {
         jshintrc: '.jshintrc'
