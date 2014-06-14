ba8f47e27b87a05aed2c89d47856f24049762db0
Andrei Rusu <andrei.rusu@beatfactor.net>
2014-06-14 23:23:08 +0200

    updated the failing test and finalized the queue refactoring

---
+++ examples/tests/nightwatch.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'Demo test Google' : function (client) {
+    client
+      .url('http://nightwatchjs.org')
+      .waitForElementVisible('body', 1000)
+      .elements('css selector', '#index-container ul.features li', function (result) {
+
+        for (var i = 0; i < result.value.length; i++) {
+          var element = result.value[i];
+
+          var selector = '#index-container ul.features li:nth-child(' + (element.ELEMENT ) +') em';
+          client.verify.cssClassPresent(selector, 'glyphicon');
+        }
+      })
+      .end();
+  }
+};
