1b709b514f6819b8c80e3a1023895cc98fc17462
Andrei Rusu <andrei.rusu@beatfactor.net>
2014-08-01 13:05:39 +0200

    refactored the url command tests

--- tests/src/testProtocolActions.js
+++ tests/src/testProtocolActions.js
@@ -909,49 +909,6 @@ module.exports = {
     });
   },
 
-  testUrlPostCommand : function(test) {
-    var protocol = this.protocol;
-    this.client.on('selenium:session_create', function(sessionId) {
-      var command = protocol.url('http://localhost');
-
-      test.equal(command.request.method, 'POST');
-      test.equal(command.data, '{"url":"http://localhost"}');
-      test.equal(command.request.path, '/wd/hub/session/1352110219202/url');
-      command.on('result', function() {
-        test.done();
-      });
-    });
-  },
-
-  testUrlGetCommand : function(test) {
-    var protocol = this.protocol;
-
-    this.client.on('selenium:session_create', function(sessionId) {
-      var command = protocol.url(function() {});
-
-      test.equal(command.request.method, 'GET');
-      test.equal(command.request.path, '/wd/hub/session/1352110219202/url');
-      command.on('result', function() {
-        test.done();
-      });
-    });
-  },
-
-  testUrlCommandCallback : function(test) {
-    var protocol = this.protocol;
-
-    this.client.on('selenium:session_create', function(sessionId) {
-      protocol.url(function() {
-        test.ok(true, 'Get callback called');
-      });
-
-      protocol.url('http://localhost', function() {
-        test.ok(true, 'Post callback called');
-        test.done();
-      });
-    });
-  },
-
   tearDown : function(callback) {
     this.client = null;
     // clean up
