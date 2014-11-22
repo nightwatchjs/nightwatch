d11e3de741a65876aa721dbcc49f636f3149e97c
Andrei Rusu <andrei.rusu@beatfactor.net>
2014-11-22 11:42:57 +0100

    added some improvements for parallell test running mode; added global disable_colors property and parallel_process_delay property to specify the delay between starting child processes

--- tests/src/runner/testParallelExecution.js
+++ tests/src/runner/testParallelExecution.js
@@ -7,23 +7,14 @@ var mockery = require('mockery');
 
 module.exports = {
   setUp: function(callback) {
+    var self = this;
+    this.allArgs = [];
+    this.allOpts = [];
     mockery.enable({ useCleanCache: true, warnOnUnregistered: false });
-    callback();
-  },
-
-  tearDown: function(callback) {
-    mockery.deregisterAll();
-    mockery.resetCache();
-    mockery.disable();
-    callback();
-  },
-
-  testParallelExecution : function(test) {
-    var allArgs = [], allOpts = [];
     mockery.registerMock('child_process', {
       execFile : function(path, args, opts) {
-        allArgs.push(args);
-        allOpts.push(opts);
+        self.allArgs.push(args);
+        self.allOpts.push(opts);
 
         function Stdout() {}
         function Stderr() {}
@@ -44,22 +35,63 @@ module.exports = {
         return new Child();
       }
     });
-
     mockery.registerMock('../lib/runner/run.js', {
       run : function(source, settings, opts, callback) {}
     });
 
+    callback();
+  },
+
+  tearDown: function(callback) {
+    mockery.deregisterAll();
+    mockery.resetCache();
+    mockery.disable();
+    callback();
+  },
+
+  testParallelExecution : function(test) {
+    var self = this;
     var CliRunner = require('../../../' + BASE_PATH + '/../lib/runner/cli/clirunner.js');
     var runner = new CliRunner({
       config : './extra/nightwatch.json',
       env : 'default,mixed'
     });
 
+    runner.init(function(output, code) {
+      test.ok(runner.isParallelMode());
+      test.equals(code, 0);
+      test.deepEqual(output, {});
+      test.equals(self.allArgs.length, 2);
+      test.equals(self.allArgs[0][2], 'default');
+      test.equals(self.allArgs[1][2], 'mixed');
+
+      test.ok('default_1' in runner.runningProcesses);
+      test.ok('mixed_2' in runner.runningProcesses);
+      test.equals(runner.runningProcesses['default_1'], false);
+      test.equals(runner.runningProcesses['mixed_2'], false);
+      test.done();
+    });
+
+    test.ok(runner.parallelMode);
+  },
+
+  testParallelExecutionSameEnv : function(test) {
+    var self = this;
+    var CliRunner = require('../../../' + BASE_PATH + '/../lib/runner/cli/clirunner.js');
+    var runner = new CliRunner({
+      config : './extra/nightwatch.json',
+      env : 'mixed,mixed'
+    });
+
     runner.init(function() {
       test.ok(runner.isParallelMode());
-      test.equals(allArgs.length, 2);
-      test.equals(allArgs[0][2], 'default');
-      test.equals(allArgs[1][2], 'mixed');
+      test.equals(self.allArgs.length, 2);
+      test.equals(self.allArgs[0][2], 'mixed');
+      test.equals(self.allArgs[1][2], 'mixed');
+      test.ok('mixed_1' in runner.runningProcesses);
+      test.ok('mixed_2' in runner.runningProcesses);
+      test.equals(runner.runningProcesses['mixed_1'], false);
+      test.equals(runner.runningProcesses['mixed_2'], false);
       test.done();
     });
 
