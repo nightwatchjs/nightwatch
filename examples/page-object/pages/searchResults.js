9c47d27ae9e90893168295db876aaff2b7825ad0
Stephanie Madison <sknopf@twitter.com>
2015-05-19 20:40:28 -0400

    Swap page object demo test from IMDB to Google Remove old Google page object example test

---
+++ examples/page-object/pages/searchResults.js
@@ -0,0 +1,31 @@
+var util = require('util')
+
+var menuXpath = '//div[contains(@class, "hdtb-mitem")][contains(., %s)]';
+
+var menuMixins = {
+  productIsSelected: function(product, callback) {
+    var self = this;
+    return this.getAttribute(product, 'class', function(result) {
+      var isSelected = result.value.indexOf('hdtb-msel') > -1;
+      callback.call(self, isSelected);
+     });
+  }
+};
+
+module.exports = {
+  elements: {
+    results: { selector: '#ires' }
+  },
+  sections: {
+    menu: {
+      selector: '#hdtb-msb',
+      mixins: [menuMixins],
+      elements: {
+        web: { selector: util.format(menuXpath, 'Web'), locateStrategy: 'xpath' },
+        video: { selector: util.format(menuXpath, 'Video'), locateStrategy: 'xpath' },
+        images: { selector: util.format(menuXpath, 'Images'), locateStrategy: 'xpath' },
+        shopping: { selector: util.format(menuXpath, 'Shopping'), locateStrategy: 'xpath' }
+      }
+    }
+  }
+};
