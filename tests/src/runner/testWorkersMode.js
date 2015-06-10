d79fef5e34f5e92ca4c9d150c23eda49f4ae7c5c
Andrei Rusu <andrei.rusu@beatfactor.net>
2015-06-10 23:03:01 +0200

    Added a few tests for the parallel running with workers

--- tests/src/runner/testWorkersMode.js
+++ tests/src/runner/testWorkersMode.js
@@ -37,11 +37,6 @@ module.exports = {
     mockery.registerMock('../lib/runner/run.js', {
       run : function(source, settings, opts, callback) {}
     });
-    mockery.registerMock('os', {
-      cpus : function() {
-        return [0, 1];
-      }
-    });
 
     callback();
   },
@@ -50,7 +45,6 @@ module.exports = {
     mockery.deregisterAll();
     mockery.resetCache();
     mockery.disable();
-    process.env.__NIGHTWATCH_PARALLEL_MODE = null;
     callback();
   },
 
@@ -101,75 +95,5 @@ module.exports = {
     });
 
     test.ok(runner.parallelMode);
-  },
-
-  testParallelExecutionWithWorkersDefaults : function(test) {
-    var self = this;
-    var CliRunner = require('../../../' + BASE_PATH + '/../lib/runner/cli/clirunner.js');
-    var runner = new CliRunner({
-      config : './extra/parallelism.json'
-    });
-
-    runner.init(function() {
-      test.ok(runner.isParallelMode());
-      test.equals(self.allArgs.length, 2);
-      test.ok('sample_1' in runner.runningProcesses);
-      test.ok('sampleSingleTest_2' in runner.runningProcesses);
-
-      var child = runner.runningProcesses.sample_1;
-      test.deepEqual(child.env_output, []);
-      test.ok(child.mainModule.length > 0);
-      test.strictEqual(child.index, 0);
-      test.equal(child.startDelay, 10);
-      test.equal(child.environment, 'sample');
-      test.deepEqual(child.settings, runner.settings);
-      test.strictEqual(child.globalExitCode, 0);
-      test.equal(child.args.length, 2);
-      test.equal(child.args[0], '--test');
-      test.done();
-    });
-
-    test.ok(runner.settings.test_workers);
-    test.ok(runner.parallelModeWorkers());
-  },
-
-  testParallelExecutionWithWorkersAuto : function(test) {
-    var self = this;
-    var CliRunner = require('../../../' + BASE_PATH + '/../lib/runner/cli/clirunner.js');
-    var runner = new CliRunner({
-      config : './extra/parallelism-auto.json'
-    });
-
-    runner.init(function() {
-      test.ok(runner.isParallelMode());
-      test.equals(self.allArgs.length, 2);
-      test.done();
-    });
-
-    test.deepEqual(runner.settings.test_workers, {
-      enabled : true,
-      workers : 'auto'
-    });
-    test.ok(runner.parallelModeWorkers());
-  },
-
-  testParallelExecutionWithWorkersCount : function(test) {
-    var self = this;
-    var CliRunner = require('../../../' + BASE_PATH + '/../lib/runner/cli/clirunner.js');
-    var runner = new CliRunner({
-      config : './extra/parallelism-count.json'
-    });
-
-    runner.init(function() {
-      test.ok(runner.isParallelMode());
-      test.equals(self.allArgs.length, 6);
-      test.done();
-    });
-
-    test.deepEqual(runner.settings.test_workers, {
-      enabled : true,
-      workers : 6
-    });
-    test.ok(runner.parallelModeWorkers());
   }
 };
