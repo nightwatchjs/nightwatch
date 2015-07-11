1ed50564a12e9fb168eaa2a647b64477f7a68637
Andrei Rusu <andrei.rusu@beatfactor.net>
2015-07-12 00:10:47 +0200

    Added support for showing original stacktraces in failure messages and some refactoring to support mocha integration

--- examples/tests/google/googleDemoTest.js
+++ examples/tests/google/googleDemoTest.js
@@ -9,9 +9,9 @@ module.exports = {
   'demo test google' : function (client) {
     client
       .url('http://google.com')
-      .waitForElementPresent('body', 1000);
-
+      .waitForElementPresent('xbody', 1000);
   },
+
   'part two' : function(client) {
     client
       .setValue('input[type=text]', ['nightwatch', client.Keys.ENTER])
