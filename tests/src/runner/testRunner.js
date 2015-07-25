9d81aca88ea53658b0cb9b08ec6b181b9456ee19
Stephanie Madison <sknopf@twitter.com>
2015-07-25 13:47:07 -0400

    Fix #574 should run afterEach with testcase cli option

--- tests/src/runner/testRunner.js
+++ tests/src/runner/testRunner.js
@@ -605,6 +605,32 @@ module.exports = {
     });
   },
 
+  testRunTestCaseWithBeforeAndAfter : function(test) {
+    var testsPath = path.join(process.cwd(), '/sampletests/before-after/syncBeforeAndAfter.js');
+    test.expect(11);
+    this.Runner.run([testsPath], {
+      seleniumPort : 10195,
+      silent : true,
+      output : false,
+      globals : {
+        test : test
+      }
+    }, {
+      output_folder : false,
+      start_session : true,
+      testcase : 'demoTestSyncOne'
+    }, function(err, results) {
+      test.equals(err, null);
+      var result = results.modules.syncBeforeAndAfter.completed;
+      test.ok('demoTestSyncOne' in result);
+      test.ok(!('beforeEach' in result));
+      test.ok(!('before' in result));
+      test.ok(!('afterEach' in result));
+      test.ok(!('after' in result));
+      test.done();
+    });
+  },
+
   testRunTestcaseInvalid : function(test) {
     var testsPath = path.join(process.cwd(), '/sampletests/before-after/syncBeforeAndAfter.js');
 
