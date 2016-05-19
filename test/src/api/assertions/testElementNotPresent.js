17fe8e99ea5d557650c06b12f6e7de1cfc1e7697
Andrei Rusu <andrei.rusu@beatfactor.net>
2016-05-19 23:45:58 +0200

    Updated tests for Fix edge elementNotPresent #836

--- test/src/api/assertions/testElementNotPresent.js
+++ test/src/api/assertions/testElementNotPresent.js
@@ -22,7 +22,36 @@ module.exports = {
         },
         assertion : function(passed, result, expected, msg, abortOnFailure) {
           assert.equal(passed, true);
-          assert.equal(result, null);
+          assert.equal(result, 'not present');
+          assert.equal(expected, 'not present');
+          assert.equal(msg, 'Testing if element <.test_element> is not present.');
+          assert.equal(abortOnFailure, true);
+          done();
+        }
+      };
+      Api.init(client);
+      var m = Api.createAssertion('elementNotPresent', assertionFn, true, client);
+      m._commandFn('.test_element');
+    },
+
+    'elementNotPresent assertion passed when exceptions are passsed' : function(done) {
+      var assertionFn = common.require('api/assertions/elementNotPresent.js');
+      var client = {
+        options : {},
+        locateStrategy : 'css selector',
+        api : {
+          elements : function(using, selector, callback) {
+            assert.equal(selector, '.test_element');
+            assert.equal(using, 'css selector');
+            callback({
+              status : -1,
+              value : null
+            });
+          }
+        },
+        assertion : function(passed, result, expected, msg, abortOnFailure) {
+          assert.equal(passed, true);
+          assert.equal(result, 'not present');
           assert.equal(expected, 'not present');
           assert.equal(msg, 'Testing if element <.test_element> is not present.');
           assert.equal(abortOnFailure, true);
