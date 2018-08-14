b1b715fe37536d61b0dc942c87e0cdeebc70ec80
Andrei Rusu <andrei.rusu@beatfactor.net>
2018-08-14 21:06:43 +0200

    added argument validation for element protocol actions which receive a web element ID

--- test/src/api/protocol/testElementCommands.js
+++ test/src/api/protocol/testElementCommands.js
@@ -30,6 +30,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdElement invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdElement',
+        args: [false, 'id', '#weblogin']
+      });
+    }, /Error: First argument passed to \.elementIdElement\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementPlural', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -52,6 +61,15 @@ describe('element actions', function () {
       commandName: 'elementIdElements',
       args: ['0', 'id', '#weblogin']
     });
+  });
+
+  it('testElementIdElementPlural invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdElements',
+        args: [false, 'id', '#weblogin']
+      });
+    }, /Error: First argument passed to \.elementIdElements\(\) should be a web element ID string\. Received boolean\./);
   });
 
   it('testElementActive', function () {
@@ -78,6 +96,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdClear invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdClear',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdClear\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdSelected', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -87,6 +114,15 @@ describe('element actions', function () {
       commandName: 'elementIdSelected',
       args: ['TEST_ELEMENT']
     });
+  });
+
+  it('testElementIdSelected invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdSelected',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdSelected\(\) should be a web element ID string\. Received boolean\./);
   });
 
   it('testElementIdEnabled', function () {
@@ -100,6 +136,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdEnabled invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdEnabled',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdEnabled\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdEquals', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -109,6 +154,15 @@ describe('element actions', function () {
       commandName: 'elementIdEquals',
       args: ['ELEMENT1', 'ELEMENT2']
     });
+  });
+
+  it('testElementIdEquals invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdEquals',
+        args: [false, 'ELEMENT2']
+      });
+    }, /Error: First argument passed to \.elementIdEquals\(\) should be a web element ID string\. Received boolean\./);
   });
 
   it('testElementIdAttribute', function () {
@@ -122,6 +176,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdAttribute invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdAttribute',
+        args: [false, 'test_attr']
+      });
+    }, /Error: First argument passed to \.elementIdAttribute\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdClick', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -131,6 +194,18 @@ describe('element actions', function () {
       commandName: 'elementIdClick',
       args: ['TEST_ELEMENT']
     });
+  });
+
+  it('testElementIdClick invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        assertion: function (opts) {
+        },
+        commandName: 'elementIdClick',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdClick\(\) should be a web element ID string\. Received boolean\./);
+
   });
 
   it('testElementIdCssProperty', function () {
@@ -144,6 +219,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdCssProperty invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdCssProperty',
+        args: [false, 'test_property']
+      });
+    }, /Error: First argument passed to \.elementIdCssProperty\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdDisplayed', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -153,6 +237,15 @@ describe('element actions', function () {
       commandName: 'elementIdDisplayed',
       args: ['TEST_ELEMENT']
     });
+  });
+
+  it('testElementIdDisplayed invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdDisplayed',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdDisplayed\(\) should be a web element ID string\. Received boolean\./);
   });
 
   it('testElementIdLocation', function () {
@@ -166,6 +259,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdLocation invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdLocation',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdLocation\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdLocationInView', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -175,6 +277,15 @@ describe('element actions', function () {
       commandName: 'elementIdLocationInView',
       args: ['TEST_ELEMENT']
     });
+  });
+
+  it('testElementIdLocationInView invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdLocationInView',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdLocationInView\(\) should be a web element ID string\. Received boolean\./);
   });
 
   it('testElementIdName', function () {
@@ -188,6 +299,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdName invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdName',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdName\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdSize', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -197,6 +317,15 @@ describe('element actions', function () {
       commandName: 'elementIdSize',
       args: ['TEST_ELEMENT']
     });
+  });
+
+  it('testElementIdSize invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdSize',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdSize\(\) should be a web element ID string\. Received boolean\./);
   });
 
   it('testElementIdText', function () {
@@ -210,6 +339,15 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdText invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        commandName: 'elementIdText',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdText\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdValueGet', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -221,6 +359,17 @@ describe('element actions', function () {
     });
   });
 
+  it('testElementIdValueGet invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        assertion: function (opts) {
+        },
+        commandName: 'elementIdValue',
+        args: [false]
+      });
+    }, /Error: First argument passed to \.elementIdValue\(\) should be a web element ID string\. Received boolean\./);
+  });
+
   it('testElementIdValuePost', function () {
     Globals.protocolTest.call(this, {
       assertion: function (opts) {
@@ -230,6 +379,17 @@ describe('element actions', function () {
       commandName: 'elementIdValue',
       args: ['TEST_ELEMENT', 'test']
     });
+  });
+
+  it('testElementIdValuePost invalid element ID', function () {
+    assert.throws(() => {
+      Globals.protocolTest.call(this, {
+        assertion: function (opts) {
+        },
+        commandName: 'elementIdValue',
+        args: [false, 'test']
+      });
+    }, /Error: First argument passed to \.elementIdValue\(\) should be a web element ID string\. Received boolean\./);
   });
 
   it('testElementWithCallbackAndContext', function (done) {
