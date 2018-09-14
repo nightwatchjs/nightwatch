5a2071bd391c113ef8ce782a9107199b8ee159a7
Andrei Rusu <andrei.rusu@beatfactor.net>
2018-09-14 09:55:34 +0200

    Fixed #1881 - .click() and .clearValue() when using geckodriver directly

---
+++ test/src/api/protocol/testElementActions.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const Globals = require('../../../lib/globals.js');
+
+describe('element W3C Webdriver actions', function() {
+  before(function() {
+    Globals.protocolBefore.call(this);
+  });
+
+  it('test click() with W3C WebDriver', function() {
+    return Globals.protocolTestWebdriver.call(this, {
+      assertion: function(opts) {
+        assert.equal(opts.method, 'POST');
+        assert.deepEqual(opts.data, {
+          id: 'abc-12345'
+        });
+        assert.equal(opts.path, '/session/1352110219202/element/abc-12345/click');
+      },
+      commandName: 'elementIdClick',
+      args: ['abc-12345']
+    });
+  });
+
+  it('test clearValue() with W3C WebDriver', function() {
+    return Globals.protocolTestWebdriver.call(this, {
+      assertion: function(opts) {
+        assert.equal(opts.method, 'POST');
+        assert.deepEqual(opts.data, {
+          id: 'abc-12345'
+        });
+        assert.equal(opts.path, '/session/1352110219202/element/abc-12345/clear');
+      },
+      commandName: 'elementIdClear',
+      args: ['abc-12345']
+    });
+  });
+});
