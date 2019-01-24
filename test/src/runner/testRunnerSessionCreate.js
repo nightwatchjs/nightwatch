322caccd9ee55a0966bdd68e0765fe003f843d91
Lacy Morrow <me@lacymorrow.com>
2019-01-24 04:08:44 -0800

    Correct spelling from sreenshot -> screenshot (#1999)
    Correct spelling from sreenshot -> screenshot

--- test/src/runner/testRunnerSessionCreate.js
+++ test/src/runner/testRunnerSessionCreate.js
@@ -86,7 +86,7 @@ describe('testRunnerSessionCreate', function() {
         assert.ok(Object.keys(results.modules).includes(`async${sep}test${sep}sample`));
         assert.ok(Object.keys(results.modules).includes(`simple${sep}test${sep}sample`));
         assert.ok(results.lastError instanceof Error);
-        assert.ok(results.lastError.message.includes('An occurred error while retrieving a new session: ' +
+        assert.ok(results.lastError.message.includes('An error occurred while retrieving a new session: ' +
           '"Session is already started"'));
       }
     };
@@ -123,7 +123,7 @@ describe('testRunnerSessionCreate', function() {
         assert.strictEqual(results.lastError.sessionConnectionRefused, true);
         assert.ok(results.lastError instanceof Error);
         assert.equal(results.lastError.code, 'ECONNREFUSED');
-        assert.ok(results.lastError.message.includes('An occurred error while retrieving a new session: "Connection refused to 127.0.0.1:1000". If the Webdriver/Selenium service is managed by Nightwatch, check if "start_process" is set to "true".'));
+        assert.ok(results.lastError.message.includes('An error occurred while retrieving a new session: "Connection refused to 127.0.0.1:1000". If the Webdriver/Selenium service is managed by Nightwatch, check if "start_process" is set to "true".'));
       }
     };
 
