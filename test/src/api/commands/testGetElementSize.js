fd10c9b98419a940cda81766ccdd0fb7417c3ccb
Andrei Rusu <andrei.rusu@beatfactor.net>
2019-10-11 23:41:10 +0200

    Fixed getElementSize and getLocation commands for w3c webdriver clients

--- test/src/api/commands/testGetElementSize.js
+++ test/src/api/commands/testGetElementSize.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const MockServer = require('../../../lib/mockserver.js');
+const Nightwatch = require('../../../lib/nightwatch.js');
 const CommandGlobals = require('../../../lib/globals/commands.js');
 
 describe('getElementSize', function() {
@@ -29,6 +30,42 @@ describe('getElementSize', function() {
     });
 
     this.client.start(done);
+  });
+
+  it('client.getElementSize() with webdriver protocol', function(done) {
+    Nightwatch.initClient({
+      selenium : {
+        version2: false,
+        start_process: false
+      },
+      webdriver:{
+        start_process: true
+      },
+    }).then(client => {
+      MockServer.addMock({
+        url: '/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/rect',
+        method: 'GET',
+        response: {
+          value: {
+            x: 341.5,
+            y: 340.95001220703125,
+            width: 683,
+            height: 60
+          }
+        }
+      }, true);
+
+      client.api.getElementSize('#webdriver', function(result) {
+        assert.deepStrictEqual(result.value, {
+          x: 341.5,
+          y: 340.95001220703125,
+          width: 683,
+          height: 60
+        });
+      });
+
+      client.start(done);
+    });
   });
 
 });
