1de2b1f3fa918062472ae7ae98bd4e7597db4dd7
Clayton Astrom <clayton.astrom@morningstar.com>
2020-06-16 10:14:39 -0500

    Refactored cli callback (thanks codekie!)

--- bin/runner.js
+++ bin/runner.js
@@ -8,26 +8,20 @@ Nightwatch.cli(async function(argv) {
   argv._source = argv['_'].slice(0);
 
   const runner = Nightwatch.CliRunner(argv);
-  await runner.setup().then(() => {
-    runner.startWebDriver()
-      .catch(err => {
-        throw err;
-      })
-      .then(() => {
-        return runner.runTests();
-      })
-      .catch(err => {
-        runner.processListener.setExitCode(10);
-      })
-      .then(() => {
-        return runner.stopWebDriver();
-      })
-      .catch(err => {
-        Logger.error(err);
-      });
+  await runner.setup().catch(err => {
+    err.message = 'An error occurred while trying to start the Nightwatch Runner: ' + err.message;
+    Logger.error(err);
+    process.exit(2);
   });
-}).catch(err => {
-  err.message = 'An error occurred while trying to start the Nightwatch Runner: ' + err.message;
-  Logger.error(err);
-  process.exit(2);
+  try {
+    try {
+      await runner.startWebDriver();
+      await runner.runTests();
+    } catch (err) {
+      runner.processListener.setExitCode(10);
+    }
+    await runner.stopWebDriver();
+  } catch (err) {
+    Logger.log(err);
+  }
 });
