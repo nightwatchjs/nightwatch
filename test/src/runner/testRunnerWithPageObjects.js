16cfa8e6db4b75e4c6d8894e25e12979c02b316e
Andrei Rusu <beatfactor@users.noreply.github.com>
2021-03-29 14:29:07 +0200

    Fixed #2624 - use locate strategy from config as default for page object elements

---
+++ test/src/runner/testRunnerWithPageObjects.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const assert = require('assert');
+const common = require('../../common.js');
+const MockServer = require('../../lib/mockserver.js');
+const CommandGlobals = require('../../lib/globals/commands.js');
+const NightwatchClient = common.require('index.js');
+
+describe('testRunner with page objects', function() {
+  before(function(done) {
+    this.server = MockServer.init();
+
+    this.server.on('listening', () => {
+      done();
+    });
+  });
+
+  after(function(done) {
+    CommandGlobals.afterEach.call(this, done);
+  });
+
+  beforeEach(function() {
+    process.removeAllListeners('exit');
+    process.removeAllListeners('uncaughtException');
+    process.removeAllListeners('unhandledRejection');
+  });
+
+  it('test Runner with useXpath and PO element selectors', function() {
+    const testsPath = path.join(__dirname, '../../sampletests/withpageobjects');
+    const globals = {
+      waitForConditionPollInterval: 50,
+
+      reporter(results) {
+        if (results.lastError) {
+          throw results.lastError;
+        }
+
+        assert.strictEqual(results.passed, 1);
+        assert.strictEqual(results.failed, 0);
+        assert.strictEqual(results.errors, 0);
+      }
+    };
+
+    return NightwatchClient.runTests(testsPath, {
+      selenium: {
+        port: 10195,
+        version2: true,
+        start_process: true
+      },
+      page_objects_path: [path.join(__dirname, '../../extra/pageobjects/pages')],
+      output: false,
+      silent: false,
+      persist_globals: true,
+      globals,
+      output_folder: false
+    });
+  });
+
+
+});
