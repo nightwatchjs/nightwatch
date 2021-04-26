1352822b33d23a1000bfd121a57aab2518646498
Dharin <dharin@browserstack.com>
2021-04-26 18:14:12 +0530

    add test

---
+++ test/src/api/commands/testSetPassword.js
@@ -0,0 +1,48 @@
+const assert = require('assert');
+const MockServer  = require('../../../lib/mockserver.js');
+const CommandGlobals = require('../../../lib/globals/commands.js');
+
+describe('setPassword', function() {
+
+  before(function(done) {
+    CommandGlobals.beforeEach.call(this, done);
+  });
+
+  after(function(done) {
+    CommandGlobals.afterEach.call(this, done);
+  });
+
+  it('client.setValue()', function(done) {
+    MockServer.addMock({
+      url : '/wd/hub/session/1352110219202/element/0/value',
+      method:'POST',
+      postdata : {value:[ 'p', 'a', 's', 's', 'w', 'o', 'r', 'd' ]},
+      response : {
+        sessionId: '1352110219202',
+        status:0
+      }
+    });
+
+    this.client.api
+      .setPassword('css selector', '#weblogin', 'password', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      })
+      .setValue('css selector', {
+        selector: '#weblogin',
+        timeout: 100
+      }, 'password', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      })
+      .setPassword({
+        selector: '#weblogin',
+        timeout: 100
+      }, 'password', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      })
+      .setPassword('#weblogin', 'password', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      });
+
+    this.client.start(done);
+  });
+});
