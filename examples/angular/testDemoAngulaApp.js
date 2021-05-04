6dce44642694a2ee4a1a25788dcf5c19bd411e53
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2021-05-04 23:18:15 +0530

    Add: one more scenario around history

--- examples/angular/testDemoAngulaApp.js
+++ examples/angular/testDemoAngulaApp.js
@@ -1,14 +1,24 @@
 describe('Angular Demo Example', () => {
-  beforeEach((browser) => browser.url('https://vaibhavsingh97.com/angular-demo/'));
+  const firstVariable = "input[ng-model='first']";
+  const secondVariable = "input[ng-model='second']";
+
+  beforeEach((browser) => {
+    browser.url('https://vaibhavsingh97.com/angular-demo/').waitForElementVisible('body');
+  });
+
+  let addvalues = (browser, a, b) => {
+    browser.setValue(firstVariable, a);
+    browser.setValue(secondVariable, b);
+    browser.click('#gobutton');
+  };
 
   test('should have a title', (browser) => {
-    browser.waitForElementVisible('body').assert.titleContains('Super Calculator');
+    browser.assert.titleContains('Super Calculator');
   });
 
   test('should add one and two', (browser) => {
-    browser
-      .waitForElementVisible('body')
-      .assert.visible("input[ng-model='first']")
+    browser.assert
+      .visible("input[ng-model='first']")
       .assert.visible("input[ng-model='second']")
       .setValue("input[ng-model='first']", 1)
       .setValue("input[ng-model='second']", 2)
@@ -18,11 +28,20 @@ describe('Angular Demo Example', () => {
 
   test('should read the value from both the input', (browser) => {
     browser
-      .waitForElementVisible('body')
       .setValue("input[ng-model='first']", 5)
       .setValue("input[ng-model='second']", 4)
       .assert.attributeContains("input[ng-model='first']", 'value', 5)
       .assert.attributeContains("input[ng-model='second']", 'value', 4);
+  });
+
+  test('should have a history', (browser) => {
+    addvalues(browser, 10, 10);
+    browser.assert.containsText('h2.ng-binding', 20);
+    addvalues(browser, 20, 20);
+    browser.assert.containsText('h2.ng-binding', 40);
+    addvalues(browser, 30, 30);
+    browser.assert.containsText('h2.ng-binding', 60);
+    browser.expect.elements('tr[ng-repeat="result in memory"]').count.to.equal(3);
   });
 
   after((browser) => browser.end());
