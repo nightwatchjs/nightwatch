b8f00272ad86132c0d7ac4f8252471457ca20c40
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2021-05-04 21:34:00 +0530

    Refactor: tests and added one more scenario

--- examples/angular/testDemoAngulaApp.js
+++ examples/angular/testDemoAngulaApp.js
@@ -1,14 +1,12 @@
 describe('Angular Demo Example', () => {
+  beforeEach((browser) => browser.url('https://vaibhavsingh97.com/angular-demo/'));
+
   test('should have a title', (browser) => {
-    browser
-      .url('https://vaibhavsingh97.com/angular-demo/')
-      .waitForElementVisible('body')
-      .assert.titleContains('Super Calculator');
+    browser.waitForElementVisible('body').assert.titleContains('Super Calculator');
   });
 
   test('should add one and two', (browser) => {
     browser
-      .url('https://vaibhavsingh97.com/angular-demo/')
       .waitForElementVisible('body')
       .assert.visible("input[ng-model='first']")
       .assert.visible("input[ng-model='second']")
@@ -16,6 +14,15 @@ describe('Angular Demo Example', () => {
       .setValue("input[ng-model='second']", 2)
       .click('#gobutton')
       .assert.containsText('h2.ng-binding', 3);
+  });
+
+  test('should read the value from both the input', (browser) => {
+    browser
+      .waitForElementVisible('body')
+      .setValue("input[ng-model='first']", 5)
+      .setValue("input[ng-model='second']", 4)
+      .assert.attributeContains("input[ng-model='first']", 'value', 5)
+      .assert.attributeContains("input[ng-model='second']", 'value', 4);
   });
 
   after((browser) => browser.end());
