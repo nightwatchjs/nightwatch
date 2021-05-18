c64ce2a9484ae8a5529e5d208e32cd2e5a1cea7c
Vaibhav Singh <singh.vaibhav2011@gmail.com>
2021-05-18 15:08:53 +0530

    Added suggested changes

--- examples/angular/testDemoAngulaApp.js
+++ examples/angular/testDemoAngulaApp.js
@@ -2,14 +2,14 @@ describe('Angular Demo Example', () => {
   const todoInputField = 'input[ng-model="todoList.todoText"]';
 
   before((browser) => {
-    browser.url('https://angularjs.org/').waitForElementVisible('body');
+    browser.url('https://angularjs.org/');
   });
 
-  test('should have a title', (browser) => {
+  it('should have a title', (browser) => {
     browser.assert.titleContains('AngularJS — Superheroic JavaScript MVW Framework');
   });
 
-  test('should add a todo', (browser) => {
+  it('should add a todo', (browser) => {
     browser.assert
       .visible(todoInputField)
       .setValue(todoInputField, 'My first todo from Nightwatch')
@@ -20,22 +20,28 @@ describe('Angular Demo Example', () => {
     browser.expect.elements('.done-true').count.to.equal(2);
   });
 
-  test('should archive completed todo', (browser) => {
-    browser.refresh();
-    browser.assert
-      .visible(todoInputField)
-      .setValue(todoInputField, 'My first todo from Nightwatch')
-      .click('input[value="add"]');
-    browser.elements('css selector', 'li[ng-repeat="todo in todoList.todos"]:not(:first-child)', (elements) => {
-      elements.value.forEach((element) => {
+  it('should archive completed todo', async (browser) => {
+    await browser.refresh();
+
+    await browser.assert.visible(todoInputField);
+
+    await browser.setValue(todoInputField, 'My first todo from Nightwatch');
+    await browser.click('input[value="add"]');
+
+    const elements = await browser.elements('css selector', 'li[ng-repeat="todo in todoList.todos"]:not(:first-child)');
+
+    await Promise.all(
+      elements.value.map((element) => {
         let key = Object.keys(element)[0];
         let elementID = element[key];
-        browser.elementIdClick(elementID);
-      });
-      browser.expect.elements('.done-true').count.to.equal(3);
-      browser.click('a[ng-click="todoList.archive()"]');
-      browser.expect.elements('li[ng-repeat="todo in todoList.todos"]').count.to.equal(0);
-    });
+
+        return browser.elementIdClick(elementID);
+      })
+    );
+
+    browser.expect.elements('.done-true').count.to.equal(3);
+    await browser.click('a[ng-click="todoList.archive()"]');
+    browser.expect.elements('li[ng-repeat="todo in todoList.todos"]').count.to.equal(0);
   });
 
   after((browser) => browser.end());
