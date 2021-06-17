1f4a790d48b415710c4ab20e764c997afb4e8d4c
Ravi Sawlani <ravisawlani04@gmail.com>
2021-06-17 12:20:21 +0530

    refactor testSelected assertion

--- test/src/api/assertions/testSelected.js
+++ test/src/api/assertions/testSelected.js
@@ -11,7 +11,7 @@ describe('assert.selected', function () {
 
   it('selected assertion passed', function () {
     return assertionTest({
-      args: ['.test_element', 'Test message'],
+      args: [{selector: '.test_element'}, 'Test message'],
       commandResult: {
         status: 0,
         value: true
@@ -71,10 +71,7 @@ describe('assert.selected', function () {
         assert.strictEqual(instance.hasFailure(), false);
         assert.strictEqual(instance.getValue(), true);
         assert.strictEqual(instance.getActual(), 'selected');
-        assert.strictEqual(
-          err.message,
-          `Error while running "selected" command: Testing if element <.test_element> is not selected in 5ms - expected "is not selected" but got: "selected" (${instance.elapsedTime}ms)`
-        );
+        assert.match(err.message, new RegExp(`Testing if element <.test_element> is not selected in 5ms - expected "is not selected" but got: "selected" \\(${instance.elapsedTime}ms\\)`));
       }
     });
   });
@@ -162,10 +159,7 @@ describe('assert.selected', function () {
           failure,
           'Expected "is selected" but got: "element could not be located"'
         );
-        assert.strictEqual(
-          err.message,
-          `Error while running "selected" command: Testing if element <.test_element> is selected in 5ms - expected "is selected" but got: "element could not be located" (${instance.elapsedTime}ms)`
-        );
+        assert.match(err.message, new RegExp(`Testing if element <.test_element> is selected in 5ms - expected "is selected" but got: "element could not be located" \\(${instance.elapsedTime}ms\\)`));
       }
     });
   });
