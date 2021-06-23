528de93bd1aaaf266608497895ea8a59da6b58a2
Ravi Sawlani <ravisawlani04@gmail.com>
2021-06-23 17:13:39 +0530

    added tests for headless,edgeOption object and windowSize options

--- test/src/index/testEdgeOptions.js
+++ test/src/index/testEdgeOptions.js
@@ -4,6 +4,23 @@ const EdgeOptions =  require('selenium-webdriver/edge').Options;
 
 describe('Test edge option', function(){
   
+  it.only('Edge option object with headless', function(){
+    const edgeoptions =  new EdgeOptions();
+    edgeoptions.headless();
+    const client = Nightwatch.createClient({
+      webdriver: {
+        options: edgeoptions
+      },
+      desiredCapabilities: {
+        browserName: 'edge'
+      }
+    });
+    const options =  client.transport.createOptions();
+
+    assert.strictEqual(options instanceof EdgeOptions, true);
+    assert.deepStrictEqual(options.options_.args, ['headless']);
+  });
+  
   it('Edge Binary Path option', function(){
     const client = Nightwatch.createClient({
       webdriver: {
@@ -32,6 +49,36 @@ describe('Test edge option', function(){
 
     assert.strictEqual(options instanceof EdgeOptions, true);
     assert.strictEqual(options.options_.logPath, '/Nightwatch/EdgeLog/');
+
+  });
+
+  it('headless option', function(){
+    const client = Nightwatch.createClient({
+      desiredCapabilities: {
+        browserName: 'edge'
+      }
+    });
+    const options = client.transport.createOptions({headless: true});
+
+    assert.strictEqual(options instanceof EdgeOptions, true);
+    assert.deepStrictEqual(options.options_.args, ['headless']);
+
+
+  });
+
+  it('window size option', function(){
+    const client =  Nightwatch.createClient({
+      window_size: {
+        height: 100,
+        width: 100
+      },
+      desiredCapabilities: {
+        browserName: 'edge'
+      }
+    });
+    const options =  client.transport.createOptions();
+    assert.strictEqual(options instanceof EdgeOptions, true);
+    assert.deepStrictEqual(options.options_.args, ['window-size=100,100']);
 
   });
   
