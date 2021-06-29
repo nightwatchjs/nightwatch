7a29438d5b5c856680300f19bdbc06ce0cc514c1
Andrei Rusu <andrei.rusu@beatfactor.net>
2021-06-29 15:54:59 +0200

    fixed linter

--- test/extra/parallelism-workers.conf.js
+++ test/extra/parallelism-workers.conf.js
@@ -1,37 +1,37 @@
 module.exports = {
-  "src_folders" : ["./test/sampletests"],
-  "output_folder" : false,
-  "output" : true,
+  src_folders : ['./test/sampletests'],
+  output_folder : false,
+  output : true,
   webdriver: {
-    "keep_alive": true
+    keep_alive: true
   },
 
-  "test_settings": {
-    "default" : {
-      "silent" : false,
+  test_settings: {
+    default : {
+      silent : false,
       test_workers: {
-        "enabled": true,
-        "workers": 5
+        enabled: true,
+        workers: 5
       },
-      "desiredCapabilities" : {
-        "browserName" : "firefox"
+      desiredCapabilities : {
+        browserName : 'firefox'
       },
       webdriver: {
         start_process: true,
         server_path: ''
-      },
+      }
     },
-    "selenium_server" : {
-      "selenium": {
+    selenium_server : {
+      selenium: {
         start_process: false,
-        "host": "127.0.0.1"
+        host: '127.0.0.1'
       }
     },
 
-    "chrome" : {
-      "extends": "selenium_server",
-      "desiredCapabilities" : {
-        "browserName" : "chrome"
+    chrome : {
+      extends: 'selenium_server',
+      desiredCapabilities : {
+        browserName : 'chrome'
       }
     }
   }
