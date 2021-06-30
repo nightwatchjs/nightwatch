4266af8a35d5bc57dd759f178a784e03a1195103
Dharin <dharin@browserstack.com>
2021-06-30 21:15:10 +0530

    add test

--- test/src/api/commands/testUploadFile.js
+++ test/src/api/commands/testUploadFile.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const MockServer  = require('../../../lib/mockserver.js');
 const CommandGlobals = require('../../../lib/globals/commands.js');
 
-describe('setPassword', function() {
+describe('uploadFile', function() {
 
   before(function(done) {
     CommandGlobals.beforeEach.call(this, done);
@@ -12,11 +12,13 @@ describe('setPassword', function() {
     CommandGlobals.afterEach.call(this, done);
   });
 
-  it('client.setPassword()', function(done) {
+  it('client.uploadFile()', function(done) {
     MockServer.addMock({
       url : '/wd/hub/session/1352110219202/element/0/value',
       method:'POST',
-      postdata : {value:[ 'p', 'a', 's', 's', 'w', 'o', 'r', 'd' ]},
+      postdata : {value:[ 
+        '/', 'f', 'i', 'l', 'e', '.', 'j', 's'
+      ]},
       response : {
         sessionId: '1352110219202',
         status:0
@@ -24,22 +26,22 @@ describe('setPassword', function() {
     });
 
     this.client.api
-      .setPassword('css selector', '#weblogin', 'password', function callback(result) {
+      .setPassword('css selector', '#choosefile', '/file.js', function callback(result) {
         assert.strictEqual(result.status, 0);
       })
       .setPassword('css selector', {
-        selector: '#weblogin',
+        selector: '#choosefile',
         timeout: 100
-      }, 'password', function callback(result) {
+      }, '/file.js', function callback(result) {
         assert.strictEqual(result.status, 0);
       })
       .setPassword({
-        selector: '#weblogin',
+        selector: '#choosefile',
         timeout: 100
-      }, 'password', function callback(result) {
+      }, '/file.js', function callback(result) {
         assert.strictEqual(result.status, 0);
       })
-      .setPassword('#weblogin', 'password', function callback(result) {
+      .setPassword('#choosefile', '/file.js', function callback(result) {
         assert.strictEqual(result.status, 0);
       });
 
