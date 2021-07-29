babd88c815e706dbe8eb223732e76c5cc97cbf3c
Ravi Sawlani <ravisawlani04@gmail.com>
2021-07-29 13:47:13 +0530

    Fixes/find elements result value in async commands (#2820)

--- test/sampletests/es6await/webdriver/getText/getTextES6.js
+++ test/sampletests/es6await/webdriver/getText/getTextES6.js
@@ -7,10 +7,18 @@ module.exports = {
     const page = client.page.simplePageObj();
 
     const result = await client.findElements('#element-selector');
-    assert.ok(result[0]);
-    assert.ok('element-6066-11e4-a52e-4f735466cecf' in result[0]);
-    assert.strictEqual(result[0].getId(), '5cc459b8-36a8-3042-8b4a-258883ea642b');
-    assert.strictEqual(result[0]['element-6066-11e4-a52e-4f735466cecf'], '5cc459b8-36a8-3042-8b4a-258883ea642b');
+    assert.strictEqual(result.status, 0);
+    assert.ok(result.value[0]);
+    assert.ok('element-6066-11e4-a52e-4f735466cecf' in result.value[0]);
+    assert.strictEqual(result.value[0].getId(), '5cc459b8-36a8-3042-8b4a-258883ea642b');
+    assert.strictEqual(result.value[0]['element-6066-11e4-a52e-4f735466cecf'], '5cc459b8-36a8-3042-8b4a-258883ea642b');
+
+    const elementSelector = await client.findElement('#element-selector');
+    assert.strictEqual(elementSelector.status, 0);
+    assert.ok(elementSelector.value);
+    assert.ok('element-6066-11e4-a52e-4f735466cecf' in elementSelector.value);
+    assert.strictEqual(elementSelector.value.getId(), '5cc459b8-36a8-3042-8b4a-258883ea642b');
+    assert.strictEqual(elementSelector.value['element-6066-11e4-a52e-4f735466cecf'], '5cc459b8-36a8-3042-8b4a-258883ea642b');
 
     const textResult = await page.getText('#element-selector');
     assert.ok(!!textResult);
@@ -20,4 +28,6 @@ module.exports = {
     const resultSection = await page.section.signUp.getText('@help');
     assert.strictEqual(resultSection.value, 'help text value');
   }
+
+
 };
