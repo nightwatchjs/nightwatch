a41f31661bc11e50f95cc24cdd8f9aeb8464dae8
Dharin Shah <Dharin-shah@users.noreply.github.com>
2021-08-19 13:56:57 +0530

    Add uploadFile() command (#2840)

---
+++ test/src/api/commands/element/testUploadFile.js
@@ -0,0 +1,52 @@
+const assert = require('assert');
+const MockServer  = require('../../../../lib/mockserver.js');
+const CommandGlobals = require('../../../../lib/globals/commands.js');
+
+describe('uploadFile', function() {
+
+  before(function(done) {
+    CommandGlobals.beforeEach.call(this, done);
+  });
+
+  after(function(done) {
+    CommandGlobals.afterEach.call(this, done);
+  });
+
+  it('client.uploadFile()', function(done) {
+    MockServer.addMock({
+      url : '/wd/hub/session/1352110219202/element/0/value',
+      method:'POST',
+      postdata : {
+        text: '/file.js',
+        value:[ 
+        '/', 'f', 'i', 'l', 'e', '.', 'j', 's'
+      ]},
+      response : {
+        sessionId: '1352110219202',
+        status:0
+      }
+    });
+
+    this.client.api
+      .uploadFile('css selector', '#choosefile', '/file.js', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      })
+      .uploadFile('css selector', {
+        selector: '#choosefile',
+        timeout: 100
+      }, '/file.js', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      })
+      .uploadFile({
+        selector: '#choosefile',
+        timeout: 100
+      }, '/file.js', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      })
+      .uploadFile('#choosefile', '/file.js', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      });
+
+    this.client.start(done);
+  });
+});
---
+++ test/src/api/protocol/testUploadFile.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+const Globals = require('../../../lib/globals.js');
+const mockery = require('mockery');
+mockery.enable({useCleanCache: true, warnOnReplace: false, warnOnUnregistered: false});
+
+describe('uploadFile command', function () {
+  before(function () {
+    Globals.protocolBefore();
+  });
+
+  after(function() {
+    mockery.deregisterAll();
+    mockery.resetCache();
+    mockery.disable();
+  })
+
+  it('should call FileDetector Api from selenium', function () {
+    let fakeRemoteCalled = false;
+    class FakeRemote {}
+    FakeRemote.FileDetector = class {
+      constructor() {
+        fakeRemoteCalled = true;
+      }  
+    }
+    mockery.registerMock('selenium-webdriver/remote', FakeRemote);
+    return Globals.protocolTest({
+      commandName: 'uploadFile',
+      args: [
+        'css selector', '#weblogin', '/file.js',
+        function (result) {
+          assert.deepStrictEqual(result.value, null);
+          assert.deepStrictEqual(result.status, 0);
+        }
+      ],
+      assertion: function (opts) {
+        assert.ok(['findElements', 'setFileDetector', 'sendKeysToElement'].includes(opts.command));
+        if(opts == 'setFileDetector' || opts.command == 'sendKeysToElement'){
+            assert.strictEqual(fakeRemoteCalled, true);
+        }
+      }
+    });
+  });
+});
