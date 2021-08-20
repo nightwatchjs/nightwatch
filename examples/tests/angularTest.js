429623ff4adde2a54c622853c6eaa2a9c9941e61
Ravi Sawlani <ravisawlani04@gmail.com>
2021-08-21 01:29:55 +0530

    added angular example tests

---
+++ examples/tests/angularTest.js
@@ -0,0 +1,20 @@
+const {assert} = require('chai');
+
+describe('angularjs homepage todo list', async function(){
+  it('should add a todo', function(){
+    browser.url('https://angularjs.org');
+    element('[ng-model="todoList.todoText"]').sendKeys('write first protractor test');
+    element('[value="add"]').click();
+    browser.expect.elements('[ng-repeat="todo in todoList.todos"]').count.to.equal(3);
+    browser.expect.element({selector: '[ng-repeat="todo in todoList.todos"]', index: 2}).text.to.equal('write first protractor test');
+    
+
+    //Task completed: mark it as done
+    browser.findElement({selector: '[ng-repeat="todo in todoList.todos"]', index: 2}, function(todo){
+      browser.elementIdElement(todo.value.getId(), 'css selector', 'input', function(input){
+        browser.elementIdClick(input.elementId);
+      });
+    });
+    browser.expect.elements('.done-true').count.to.equal(2);
+  });
+});
