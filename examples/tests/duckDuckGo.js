509672bc25f089cf8da4ca5aa7ef974aab3660d9
Ravi Sawlani <ravisawlani04@gmail.com>
2021-08-20 16:00:06 +0530

    added duckduckGo example test

---
+++ examples/tests/duckDuckGo.js
@@ -0,0 +1,19 @@
+describe('duck duck go basic tests', function(){
+
+  const searchBar = element(by.css('#search_form_input_homepage'));
+  const searchButton =  element(by.css('#search_button_homepage'));
+  
+  test('Search Nightwatch.js and check results', function(){
+
+    browser.url('https://duckduckgo.com')
+      .waitForElementVisible(searchBar)
+      .waitForElementVisible(searchButton)
+      .setValue(searchBar, ['Nightwatch.js', browser.keys.ENTER])
+      .click(searchButton)
+      .assert.visible('.results--main')
+      .assert.containsText('.results--main', 'Nightwatch.js');
+    
+    
+
+  }); 
+});
