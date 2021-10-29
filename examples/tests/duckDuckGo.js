a07d6dea41e30b22d054ebc9b8050551520563fb
Andrei Rusu <beatfactor@users.noreply.github.com>
2021-10-29 09:38:53 +0200

    Update duckDuckGo.js

--- examples/tests/duckDuckGo.js
+++ examples/tests/duckDuckGo.js
@@ -4,11 +4,11 @@ describe('duck duck go basic tests', function(){
   const searchButton =  element(by.css('#search_button_homepage'));
   
   test('Search Nightwatch.js and check results', function(){
-
-    browser.url('https://duckduckgo.com')
+    browser
+      .url('https://duckduckgo.com')
       .waitForElementVisible('#search_form_input_homepage')
       .waitForElementVisible('#search_button_homepage')
-      .setValue(searchBar, ['Nightwatch.js', browser.keys.ENTER])
+      .sendKeys(searchBar, ['Nightwatch.js', browser.keys.ENTER])
       .click(searchButton)
       .assert.visible('.results--main')
       .assert.containsText('.results--main', 'Nightwatch.js');
