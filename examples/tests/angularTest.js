10b0cf9a7ad46fbd6d4b1c3bf902e99928736cbb
Andrei Rusu <beatfactor@users.noreply.github.com>
2021-10-29 09:38:12 +0200

    Update angularTest.js

--- examples/tests/angularTest.js
+++ examples/tests/angularTest.js
@@ -1,18 +1,24 @@
-describe('angularjs homepage todo list', function(){
-  it('should add a todo', function(){
-    browser.url('https://angularjs.org');
-    element('[ng-model="todoList.todoText"]').sendKeys('write first protractor test');
-    element('[value="add"]').click();
-    browser.expect.elements('[ng-repeat="todo in todoList.todos"]').count.to.equal(3);
-    browser.expect.element({selector: '[ng-repeat="todo in todoList.todos"]', index: 2}).text.to.equal('write first protractor test');
-    
+describe('angularjs homepage todo list', function() {
 
-    //Task completed: mark it as done
-    browser.findElement({selector: '[ng-repeat="todo in todoList.todos"]', index: 2}, function(todo){
-      browser.elementIdElement(todo.value.getId(), 'css selector', 'input', function(input){
+  // using the new element() global utility in nightwatch@2 to init elements
+  const todoElement = element('[ng-model="todoList.todoText"]');
+  const addButtonEl = element('[value="add"]');
+  
+  it('should add a todo', function() {
+    browser.navigateTo('https://angularjs.org');
+    
+    todoElement.sendKeys('what is nightwatch?');
+    addButtonEl.click();
+    
+    browser.expect.elements('[ng-repeat="todo in todoList.todos"]').count.to.equal(3);
+    browser.expect.element({selector: '[ng-repeat="todo in todoList.todos"]', index: 2}).text.to.equal('what is nightwatch?');
+    
+    browser.findElement({selector: '[ng-repeat="todo in todoList.todos"]', index: 2}, function(todo) {
+      browser.elementIdElement(todo.value.getId(), 'css selector', 'input', function(input) {
         browser.elementIdClick(input.elementId);
       });
     });
+    
     browser.expect.elements('.done-true').count.to.equal(2);
   });
 });
