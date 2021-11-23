900902b0f58cd0aed74121bab3ae8846ed6122ca
Ravi Sawlani <ravisawlani04@gmail.com>
2021-11-23 13:52:24 +0530

    added doubleClick tests

--- test/src/api/protocol/testDoubleClick.js
+++ test/src/api/protocol/testDoubleClick.js
@@ -1,20 +1,64 @@
 const assert = require('assert');
-const Globals = require('../../../lib/globals.js');
+const CommandGlobals = require('../../../lib/globals/commands-w3c.js');
 
-describe('client.doubleClick', function() {
-  before(function() {
-    Globals.protocolBefore();
+describe('client.doubleClick()', function() {
+  let callbackResult;
+
+  before(function(done) {
+    CommandGlobals.beforeEach.call(this, done);
   });
 
-  it('testDoubleClick', function() {
-    return Globals.protocolTest({
-      assertion: function(opts) {
-        assert.strictEqual(opts.method, 'POST');
-        assert.strictEqual(opts.path, '/session/1352110219202/doubleclick');
-      },
-      commandName: 'doubleClick',
-      args: []
+  after(function(done) {
+    CommandGlobals.afterEach.call(this, done);
+  });
+
+  it('test .doubleClick() - successful', function() {
+
+    this.client.transport.driver.actions = function() {
+      return {
+        doubleClick: function() {
+          return {
+            perform:  function() {
+              return Promise.resolve()
+            }
+          }
+        }
+      }
+    }
+
+    this.client.api.doubleClick(function callback(result) {
+      callbackResult = result;
+    });
+
+    return this.client.start(function() {
+      assert.ok(callbackResult, 'Result from callback is undefined');
+      assert.strictEqual(callbackResult.status, 0);
+      assert.strictEqual(callbackResult.value, null);
     });
   });
 
+  it('test .doubleClick - failed', function() {
+
+    this.client.transport.driver.actions = function() {
+      return {
+        doubleClick: function() {
+          return {
+            perform:  function() {
+              return Promise.reject(new Error('no such window'))
+            }
+          }
+        }
+      }
+    }
+
+    this.client.api.doubleClick(function callback(result) {
+      callbackResult = result;
+    });
+
+    return this.client.start(function() {
+      assert.ok(callbackResult, 'Result from callback is undefined');
+      assert.strictEqual(callbackResult.status, -1);
+      assert.strictEqual(callbackResult.error.message, 'no such window');
+    });
+  });
 });
