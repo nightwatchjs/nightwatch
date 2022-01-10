25b73337fc880f3e6f14a942eeca8e8b8dc45b3a
Harshit Agrawal <harshit.a@browserstack.com>
2022-01-11 02:59:27 +0530

    fixed lint error

--- test/src/api/commands/element/testClick.js
+++ test/src/api/commands/element/testClick.js
@@ -199,15 +199,15 @@ describe('.click()', function() {
     });
     const api = this.client.api;
     this.client.api
-    .click('#weblogin', 'Clicked weblogin element')
-    .click('css selector', '#weblogin', 'Clicked css selector')
-    .click('#weblogin', function callback(result) {
-      assert.strictEqual(result.status, 0);
-      assert.strictEqual(this, api);
-    }, 'Clicked weblogin element')
-    .click('css selector', '#weblogin', function callback(result) {
-      assert.strictEqual(result.status, 0);
-    }, 'Clicked css selector');
+      .click('#weblogin', 'Clicked weblogin element')
+      .click('css selector', '#weblogin', 'Clicked css selector')
+      .click('#weblogin', function callback(result) {
+        assert.strictEqual(result.status, 0);
+        assert.strictEqual(this, api);
+      }, 'Clicked weblogin element')
+      .click('css selector', '#weblogin', function callback(result) {
+        assert.strictEqual(result.status, 0);
+      }, 'Clicked css selector');
     
     this.client.start(done);
   });
