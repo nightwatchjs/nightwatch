0de9e5ff872947e563933a695782e08bac4fed26
Andrei Rusu <andrei.rusu@beatfactor.net>
2022-01-28 10:03:38 +0100

    updated example tests and output formatting

--- examples/tests/google.js
+++ examples/tests/google.js
@@ -24,7 +24,7 @@ describe('sample google search', function() {
       .waitForElementVisible('form[action="/search"] input[type=text]')
 
       .sendKeys('form[action="/search"] input[type=text]', ['Nightwatch.js', browser.Keys.ENTER])
-      .assert.containsText('#rso>:first-child', 'Nightwatch.js')
+      .assert.textContain('#rso>:first-child', 'Nightwatch.js')
 
       .end();
 
