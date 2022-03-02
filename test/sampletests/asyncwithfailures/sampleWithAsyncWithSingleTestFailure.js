f05316246fb39dda710ef54cd2eb375db06419ee
Andrei Rusu <andrei.rusu@beatfactor.net>
2022-03-02 21:41:49 +0100

    updated test for mocha issue with resetting the last error

--- test/sampletests/asyncwithfailures/sampleWithAsyncWithSingleTestFailure.js
+++ test/sampletests/asyncwithfailures/sampleWithAsyncWithSingleTestFailure.js
@@ -12,20 +12,6 @@ describe('Mocha tests with failures', function() {
       .end();
   });
 
-  it('demoTest2', function (browser) {
-    browser.url('http://localhost')
-      .assert.elementPresent('#weblogin')
-      .pause(100)
-      .assert.elementPresent('#badElement')
-      .end();
-  });
-
-  it('demoTest3', function (browser) {
-    browser.url('http://localhost')
-      .assert.elementPresent('#weblogin')
-      .end();
-  });
-
   afterEach(function(browser, callback) {
     browser.globals.test_calls++;
     callback();
