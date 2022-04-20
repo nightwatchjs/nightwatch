84e1d0a34e2ef0d06fc13ad3901a31bbe20584ed
Ravi Sawlani <ravisawlani04@gmail.com>
2022-04-20 17:44:36 +0530

    remove extra spaces

--- test/sampletests/asyncwithexpectfailures/sample.js
+++ test/sampletests/asyncwithexpectfailures/sample.js
@@ -1,9 +1,7 @@
-
 describe('browse.expect with failures in async step', function() {
   it('async step failure', async function() {
     browser.url('http://localhost');
     browser.expect.element('#weblogin').to.be.contain.text('some text');
-        
   });
 
   it('second step without failure', function() {
