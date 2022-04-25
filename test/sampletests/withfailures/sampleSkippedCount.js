e59a756827b385b7ae2ef456e39efa58e977cca7
Harshit Agrawal <harshit.a@browserstack.com>
2022-04-25 17:30:59 +0530

    Fixed #3127 skipped count was incorrect in global reporter results

---
+++ test/sampletests/withfailures/sampleSkippedCount.js
@@ -0,0 +1,11 @@
+module.exports = {
+  TestCase01: browser => {
+    browser.assert.ok(false)
+  },
+  TestCase02: browser => {
+    // do something
+  },
+  TestCase03: browser => {
+    // do something
+  }
+}
