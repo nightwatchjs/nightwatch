c1fd5c6ba1897fd1ffbe249ac81044869eeeacfd
Priyansh Garg <priyanshgarg30@gmail.com>
2022-05-18 00:58:58 +0530

    Add startCapturingLogs command.

---
+++ test/src/api/commands/client/testStartCapturingLogs.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+const CommandGlobals = require('../../../../lib/globals/commands.js');
+const MockServer = require('../../../../lib/mockserver.js');
+const Nightwatch = require('../../../../lib/nightwatch.js');
+
+describe('.startCapturingLogs()', function () {
+  beforeEach(function (done) {
+    CommandGlobals.beforeEach.call(this, done);
+  });
+
+  afterEach(function (done) {
+    CommandGlobals.afterEach.call(this, done);
+  });
+
+  it('browser.startCapturingLogs()', function (done) {
+
+    MockServer.addMock({
+      url: '/session',
+      response: {
+        value: {
+          sessionId: '13521-10219-202',
+          capabilities: {
+            browserName: 'chrome',
+            browserVersion: '92.0'
+          }
+        }
+      },
+      method: 'POST',
+      statusCode: 201
+    }, true);
+
+    Nightwatch.initW3CClient({
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {}
+      }
+    }).then(client => {
+
+      let expectedPageCdpConnection;
+      let expectedUserCallback;
+
+      client.transport.driver.createCDPConnection = function() {
+        return Promise.resolve();
+      };
+      client.transport.driver.onLogEvent = (pageCdpConnection, userCallback) => {
+        expectedPageCdpConnection = pageCdpConnection;
+        expectedUserCallback = userCallback;
+      };
+
+      //eslint-disable-next-line
+      const userCallback = (event) => {console.log(event)};
+      client.api.startCapturingLogs(userCallback, function () {
+        assert.strictEqual(expectedPageCdpConnection, undefined);  // pageCdpConnection is mocked
+        assert.strictEqual(expectedUserCallback, userCallback);
+      });
+
+      client.start(done);
+    });
+  });
+
+  it('browser.startCapturingLogs - driver not supported', function(done){
+    Nightwatch.initW3CClient({
+      desiredCapabilities: {
+        browserName: 'firefox'
+      }
+    }).then(client => {
+      //eslint-disable-next-line
+      const userCallback = (event) => {console.log(event)};
+
+      client.api.startCapturingLogs(userCallback, function(result){
+        assert.strictEqual(result.status, -1);
+        assert.strictEqual(result.error, 'StartCapturingLogs is not supported while using this driver');
+      });
+      client.start(done);
+    });
+  });
+
+});
