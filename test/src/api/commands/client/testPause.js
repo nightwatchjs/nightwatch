8ba9f07e4c431645b8b74211e6e305f69bae6484
Priyansh Garg <39924567+garg3133@users.noreply.github.com>
2022-07-19 22:15:32 +0530

    Add pause and debug capabilities. (#3171)

--- test/src/api/commands/client/testPause.js
+++ test/src/api/commands/client/testPause.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const CommandGlobals = require('../../../../lib/globals/commands.js');
 
 describe('.pause()', function() {
@@ -9,9 +10,126 @@ describe('.pause()', function() {
     CommandGlobals.afterEach.call(this, done);
   });
 
-  it('browser.pause()', function(done) {
+  it('browser.pause(10)', function(done) {
+    const startTime = new Date();
     this.client.api.pause(10, function() {
+      const timeElapsed = new Date() - startTime;
+      assert.ok(timeElapsed >= 10);
+      assert.ok(timeElapsed <= 20);
     });
+
+    this.client.start(done);
+  });
+
+  it('browser.pause(200)', function(done) {
+    const startTime = new Date();
+    this.client.api.pause(200, function() {
+      const timeElapsed = new Date() - startTime;
+      assert.ok(timeElapsed >= 200);
+      assert.ok(timeElapsed <= 210);
+    });
+
+    this.client.start(done);
+  });
+
+  it('browser.pause() and then resume after 200 ms', function(done) {
+    const startTime = new Date();
+    this.client.api.pause(undefined, function() {
+      const timeElapsed = new Date() - startTime;
+      assert.ok(timeElapsed >= 200);
+      assert.ok(timeElapsed <= 220);
+    });
+
+    setTimeout(() => {
+      process.stdin.emit('keypress', null, {name: 'return'});
+    }, 200);
+
+    this.client.start(done);
+  });
+
+  it('browser.pause() and then resume after 1000 ms', function(done) {
+    const startTime = new Date();
+    this.client.api.pause(undefined, function() {
+      const timeElapsed = new Date() - startTime;
+      assert.ok(timeElapsed >= 1000);
+      assert.ok(timeElapsed <= 1020);
+    });
+
+    setTimeout(() => {
+      process.stdin.emit('keypress', null, {name: 'return'});
+    }, 1000);
+
+    this.client.start(done);
+  });
+
+  it('browser.pause() and then step over using <space>', function(done) {
+    const Debuggability = require('../../../../../lib/utils/debuggability.js');
+
+    const startTime = new Date();
+
+    assert.strictEqual(Debuggability.stepOverAndPause, false);
+    this.client.api.pause(undefined, function() {
+      const timeElapsed = new Date() - startTime;
+      assert.ok(timeElapsed >= 200);
+      assert.ok(timeElapsed <= 220);
+      assert.strictEqual(Debuggability.stepOverAndPause, true);
+    });
+
+    setTimeout(() => {
+      process.stdin.emit('keypress', null, {name: 'space'});
+    }, 200);
+
+    this.client.start(done);
+  });
+
+  it('browser.pause() and then step over using F10', function(done) {
+    const Debuggability = require('../../../../../lib/utils/debuggability.js');
+
+    const startTime = new Date();
+
+    assert.strictEqual(Debuggability.stepOverAndPause, false);
+    this.client.api.pause(undefined, function() {
+      const timeElapsed = new Date() - startTime;
+      assert.ok(timeElapsed >= 200);
+      assert.ok(timeElapsed <= 220);
+      assert.strictEqual(Debuggability.stepOverAndPause, true);
+    });
+
+    setTimeout(() => {
+      process.stdin.emit('keypress', null, {name: 'f10'});
+    }, 200);
+
+    this.client.start(done);
+  });
+
+  it('browser.pause() and then exit', function(done) {
+    const clientApi = this.client.api;
+    const origEndFn = clientApi.end;
+
+    let apiEndCalledWithCallback = false;
+    let callbackCalled;
+    clientApi.end = (cb) => {
+      if (cb) {
+        apiEndCalledWithCallback = true;
+        callbackCalled = cb;
+      }
+    };
+    
+    const startTime = new Date();
+
+    this.client.api.pause(undefined, function() {
+      const timeElapsed = new Date() - startTime;
+      assert.ok(timeElapsed >= 200);
+      assert.ok(timeElapsed <= 220);
+      assert.strictEqual(apiEndCalledWithCallback, true);
+      assert.strictEqual(callbackCalled.toString().includes('process.exit(0)'), true);
+
+      clientApi.end = origEndFn;
+    });
+
+    setTimeout(() => {
+      process.stdin.emit('keypress', null, {ctrl: true, name: 'c'});
+    }, 200);
 
     this.client.start(done);
   });
