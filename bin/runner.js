4886e26c92041bee8c1f5948261118697b4fd21e
Vaibhav Singh <vaibhav.s@browserstack.com>
2022-08-22 14:49:07 +0530

    feat: Add tsconfig_path config, & nightwatch conf intelligently

--- bin/runner.js
+++ bin/runner.js
@@ -1,38 +1,18 @@
 /**
  * Module dependencies
  */
-const path = require('path');
 const Nightwatch = require('../lib/index.js');
-const {
-  Logger,
-  shouldReplaceStack,
-  alwaysDisplayError,
-  loadTSNode,
-  findTSConfigFile,
-  fileExistsSync
-} = require('../lib/utils');
+const {Logger, shouldReplaceStack, alwaysDisplayError} = require('../lib/utils');
 
 try {
   Nightwatch.cli(function (argv) {
-
-    const isTypescriptProject = fileExistsSync(path.join(process.cwd(), 'tsconfig.json'));
-    if (isTypescriptProject) {
-      const projectTsFilePath = findTSConfigFile();
-
-      if (projectTsFilePath !== '') {
-        loadTSNode(projectTsFilePath);
-      } else {
-        Logger.info('Now you can run TS tests directly using Nightwatch.\nFollow this document to learn more.');
-      }
-    }
-
     argv._source = argv['_'].slice(0);
 
     const runner = Nightwatch.CliRunner(argv);
 
     return runner
       .setupAsync()
-      .catch(err => {
+      .catch((err) => {
         if (err.code === 'ERR_REQUIRE_ESM') {
           err.showTrace = false;
         }
@@ -41,7 +21,7 @@ try {
       })
       .then(() => runner.runTests())
       .catch((err) => {
-        if (!err.displayed || alwaysDisplayError(err) && !err.displayed) {
+        if (!err.displayed || (alwaysDisplayError(err) && !err.displayed)) {
           Logger.error(err);
         }
 
