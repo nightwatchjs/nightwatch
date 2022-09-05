29c4c228a6b17b4bcef39914ca9fa1bb57c482ad
Binayak Ghosh <ghoshbinayak@gmail.com>
2022-09-05 14:10:15 +0530

    skip test for windows, not necessary

--- test/src/utils/testStackTrace.js
+++ test/src/utils/testStackTrace.js
@@ -6,6 +6,7 @@ const Utils = common.require('utils');
 const beautifyStackTrace = common.require('utils/beautifyStackTrace.js');
 const colors = common.require('utils/colors.js');
 const {Logger} = common.require('utils');
+const os = require('os');
 
 describe('test stackTrace parse', function() {
   before(() => colors.disable());
@@ -96,6 +97,13 @@ describe('test stackTrace parse', function() {
   });
 
   it('should format error log properly', function() {
+    // Skip test for windows
+    if (os.platform === 'win32') {
+      assert.ok(true);
+          
+      return;
+    }
+    
     colors.enable();
     const errorFilePath = path.join(__dirname, '../../sampletests/unknown-method/UnknownMethod.js');
     const lineNumber  = 4;
