2a30d33e3e9c2debb61f54793465c4b10f77c8d3
Andrei Rusu <andrei.rusu@beatfactor.net>
2022-09-23 16:23:12 +0200

    fixed eslint

--- examples/tests/ecosia.js
+++ examples/tests/ecosia.js
@@ -1,13 +1,13 @@
 describe('Ecosia.org Demo', function() {
 
   before(browser => {
-		browser
-			.navigateTo('https://www.ecosia.org/');
+    browser
+      .navigateTo('https://www.ecosia.org/');
   });
 
   it('Demo test ecosia.org', function(browser) {
     browser
-			.waitForElementVisible('body')
+      .waitForElementVisible('body')
       .assert.titleContains('Ecosia')
       .assert.visible('input[type=search]')
       .setValue('input[type=search]', 'nightwatch')
