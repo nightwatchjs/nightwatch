724f3a3b85494628ead701d30e3aa7720de3d09a
Prudhvi <prudhvi@browserstack.com>
2022-11-01 20:19:26 +0530

    added test for 127.0.0.1

--- test/src/index/testMobileComponent.js
+++ test/src/index/testMobileComponent.js
@@ -3,10 +3,25 @@ const common = require('../../common.js');
 const NightwatchClient = common.require('index.js');
 
 
-describe('Mobile Component Testing', function () {
-  it('test baseUrl for Andoid Emulator',  function(){
+describe('Mobile Component Testing in Android Emulator', function () {
+  it('test baseUrl - localhost',  function(){
     const client = NightwatchClient.client({
       baseUrl: 'http://localhost:3000',
+      webdriver: {
+        start_process: true
+      },
+      desiredCapabilities: {
+        avd: 'nightwatch-android-11',
+        real_mobile: false
+      }
+    });
+
+    assert.strictEqual(client.api.baseUrl, 'http://10.0.2.2:3000');
+  });
+
+  it('test baseUrl - 127.0.0.1',  function(){
+    const client = NightwatchClient.client({
+      baseUrl: 'http://127.0.0.1:3000',
       webdriver: {
         start_process: true
       },
