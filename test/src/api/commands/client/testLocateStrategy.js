8b5796d883e5292cb515cf30044fe95f3a85915a
Harshit Agrawal <harshit.a@browserstack.com>
2023-01-16 19:24:56 +0530

    test case - strategy switched without useXpath, useCss

--- test/src/api/commands/client/testLocateStrategy.js
+++ test/src/api/commands/client/testLocateStrategy.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const CommandGlobals = require('../../../../lib/globals/commands.js');
+const MockServer  = require('../../../../lib/mockserver.js');
 
 describe('Locate strategies', function() {
   before(function(done) {
@@ -23,6 +24,48 @@ describe('Locate strategies', function() {
     let client = this.client;
     this.client.api.useCss(function() {
       assert.strictEqual(client.locateStrategy, 'css selector');
+    });
+
+    this.client.start(done);
+  });
+
+  it.only('strategy switched without useXpath()', function(done) {
+    MockServer.addMock({
+      url: '/wd/hub/session/1352110219202/elements',
+      postdata: {
+        using: 'xpath',
+        value: '//*'
+      },
+      response: JSON.stringify({
+        sessionId: '1352110219202',
+        status: 0,
+        value: [{'ELEMENT': '0'}]
+      })
+    });
+
+    MockServer.addMock({
+      url: '/wd/hub/session/1352110219202/elements',
+      postdata: {
+        using: 'css selector',
+        value: '*'
+      },
+      response: JSON.stringify({
+        sessionId: '1352110219202',
+        status: 0,
+        value: [{'ELEMENT': '1'}]
+      })
+    });
+
+    this.client.api.findElement('//*', function(result, instance) {
+      assert.strictEqual(instance.strategy, 'xpath');
+      assert.strictEqual(result.status, 0);
+      assert.strictEqual(result.value.getId(), '0');
+    });
+
+    this.client.api.findElement('*', function(result, instance) {
+      assert.strictEqual(instance.strategy, 'css selector');
+      assert.strictEqual(result.status, 0);
+      assert.strictEqual(result.value.getId(), '1');
     });
 
     this.client.start(done);
