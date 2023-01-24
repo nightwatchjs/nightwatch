5ae53f7069aa003eca070056b5b2cafb327a255e
Ravi Sawlani <ravisawlani04@gmail.com>
2023-01-24 20:34:48 +0530

    fix test

--- test/lib/command-mocks.js
+++ test/lib/command-mocks.js
@@ -385,7 +385,7 @@ module.exports = {
     url = '/wd/hub/session'
   }) {
     const browserName = 'chrome';
-    const headlessOpt = headless ? 'headless=chrome' : '';
+    const headlessOpt = headless ? 'headless=new' : '';
     const options = {
       ['goog:chromeOptions']: {}
     };
