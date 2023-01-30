2d86d95736039e4ecfbeba27fe67c397566c4b15
Harshit Agrawal <harshit.a@browserstack.com>
2023-01-30 20:30:07 +0530

    added test for android

--- test/src/index/transport/testMobileWebSupport.js
+++ test/src/index/transport/testMobileWebSupport.js
@@ -6,10 +6,14 @@ const MockServer = require('../../../lib/mockserver.js');
 const {settings} = common;
 const {runTests} = common.require('index.js');
 const Transport = require('../../../../lib/transport/selenium-webdriver/index.js');
-const {IosSessionNotCreatedError} = common.require('utils/mobile.js');
+const {IosSessionNotCreatedError, AndroidConnectionError} = common.require('utils/mobile.js');
+const mockery = require('mockery');
 
-
-describe('MobileSupport', function () {
+describe.only('MobileSupport', function () {
+  before(function () {
+    mockery.enable({useCleanCache: true, warnOnReplace: false, warnOnUnregistered: false});
+  })
+  
   beforeEach(function (done) {
     this.getDriverBackup = Transport.prototype.getDriver;
     this.createDriverServiceBackup = Transport.prototype.createDriverService;
@@ -25,6 +29,9 @@ describe('MobileSupport', function () {
   });
 
   afterEach(function (done) {
+    mockery.deregisterAll();
+    mockery.resetCache();
+    mockery.disable();
     Transport.prototype.getDriver = this.getDriverBackup;
     Transport.prototype.createDriverService = this.createDriverServiceBackup;
 
@@ -158,5 +165,70 @@ describe('MobileSupport', function () {
     assert.strictEqual(runner.test_settings.desiredCapabilities['safari:deviceUDID'], '00008030-00024C2C3453402E');
     assert.strictEqual(runner.test_settings.desiredCapabilities.platformName, 'iOS');
     assert.ok(!runner.test_settings.desiredCapabilities['safari:useSimulator']);
+  });
+
+  it('error classes for mobile-web support - AndroidConnectionError', function () {
+    this.timeout(10000);
+    mockery.enable({useCleanCache: true, warnOnReplace: false, warnOnUnregistered: false});
+
+    mockery.registerMock('../androidEmulator.js', class AndroidServer {
+      constructor() {}
+
+      launchEmulator() {
+        return Promise.resolve();
+      }
+
+      killEmulator() {}
+    });
+
+    Transport.prototype.getDriver = function() {
+      const err = new Error('unknown error: There are no devices online');
+      err.name = 'SessionNotCreatedError';
+
+      throw err;
+    };
+
+    mockery.registerMock('./', Transport);
+    mockery.registerMock('@nightwatch/mobile-helper', {
+      getBinaryLocation(){}, getPlatformName(){}
+    });
+
+    let src_folders = [
+      path.join(__dirname, '../../../sampletests/withsubfolders/simple')
+    ];
+
+    let globals = {
+      calls: 0,
+      retryAssertionTimeout: 0,
+      reporter(results, cb) {
+        assert.ok(results.lastError instanceof Error);
+        assert.ok(results.lastError instanceof AndroidConnectionError);
+        assert.ok(results.lastError.message.includes('no devices online'));
+        assert.ok(Object.prototype.hasOwnProperty.call(results.lastError, 'message'));
+        assert.ok(results.lastError.help.length, 4);
+        cb();
+      }
+    };
+
+    return runTests({}, settings({
+      output: false,
+      src_folders,
+      globals,
+      desiredCapabilities: {
+        real_mobile: false,
+        browserName: 'chrome',
+        avd: 'dummy',
+        'goog:chromeOptions': {
+          androidPackage: 'com.android.chrome'
+        }
+      },
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // --verbose
+        ]
+      }
+    }))
   });
 });
