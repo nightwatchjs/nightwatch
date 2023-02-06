01df15ba6a2ecacc43caff56ca65b6f0db68f775
Harshit Agrawal <harshit.a@browserstack.com>
2023-02-06 23:28:49 +0530

    test fix

--- test/src/cli/testCliRunnerSequential.js
+++ test/src/cli/testCliRunnerSequential.js
@@ -4,7 +4,7 @@ const path = require('path');
 const assert = require('assert');
 const common = require('../../common.js');
 
-describe('test Sequential Execution', function() {
+describe.only('test Sequential Execution', function() {
   const allArgs = [];
   const allOpts = [];
 
@@ -87,7 +87,8 @@ describe('test Sequential Execution', function() {
     process.chdir(originalCwd);
   });
 
-  it('testSequentialExecution - sequential with multiple environment', function() {
+  it('testSequentialExecution - sequential with multiple environment', async function() {
+    this.timeout(10000);
     const CliRunner = common.require('runner/cli/cli.js');
     let originalCwd = process.cwd();
     process.chdir(path.join(__dirname, '../../extra/'));
@@ -105,5 +106,11 @@ describe('test Sequential Execution', function() {
     assert.ok(runner.sequentialMode());
     assert.strictEqual(runner.testEnv, 'default,mixed');
     assert.deepStrictEqual(runner.availableTestEnvs, ['default', 'mixed']);
+
+    await runner.runTests();
+
+    assert.ok(runner.sequentialMode());
+    assert.deepEqual(runner.testEnvArray, ['default', 'mixed']);
+    process.chdir(originalCwd);
   });
 });
