2e8eb5c6d13497d0e85797aa0c0a06d71eb85f66
Priyansh Garg <priyanshgarg30@gmail.com>
2023-03-12 22:50:35 +0530

    Update a11y command.

--- test/src/api/commands/client/testAxeCommands.js
+++ test/src/api/commands/client/testAxeCommands.js
@@ -24,7 +24,7 @@ describe('accessibility commands', function () {
     assert.strictEqual(typeof this.client.api.axeRun, 'function');
   });
 
-  it('test .a11y.runTests()', function (done) {
+  it('test .a11y.runTests()', async function () {
     // nock for axeInject
     nock('http://localhost:10195')
       .post('/session/13521-10219-202/execute/sync')
@@ -48,14 +48,16 @@ describe('accessibility commands', function () {
         };
       });
     
-    this.client.api.a11y.runTests('body', {runOnly: ['color-contrast', 'image-alt']}, function (result) {
-      assert.deepStrictEqual(result, {});
+    let axeResult;
+    await this.client.api.a11y.runTests('body', {runOnly: ['color-contrast', 'image-alt']}, function (result) {
+      axeResult = result;
     });
+    await this.client.start();
 
-    this.client.start(done);
+    assert.deepStrictEqual(axeResult, {});
   });
 
-  it('test .a11y.runTests() without options', function (done) {
+  it('test .a11y.runTests() without options', async function () {
     // nock for axeInject
     nock('http://localhost:10195')
       .post('/session/13521-10219-202/execute/sync')
@@ -79,24 +81,45 @@ describe('accessibility commands', function () {
         };
       });
     
-    this.client.api.a11y.runTests('body', function (result) {
-      assert.deepStrictEqual(result, {});
+    let axeResult;
+    await this.client.api.a11y.runTests('body', function (result) {
+      axeResult = result;
     });
+    await this.client.start();
 
-    this.client.start(done);
+    assert.deepStrictEqual(axeResult, {});
   });
 
-  it('test .a11y.runTests() without any argument', function (done) {
-    this.client.api.a11y.runTests();
+  it('test .a11y.runTests() without any argument', async function () {
+    // nock for axeInject
+    nock('http://localhost:10195')
+      .post('/session/13521-10219-202/execute/sync')
+      .reply(200, function () {
+        return {
+          value: null
+        };
+      });
 
-    this.client.start(function(err) {
-      try {
-        assert.ok(err instanceof Error);
-        assert.strictEqual(err.message.includes('First argument passed to .a11y.runTests() must be a selector string.'), true);
-        done();
-      } catch (err) {
-        done(err);
-      }
+    // nock for axeRun
+    nock('http://localhost:10195')
+      .post('/session/13521-10219-202/execute/async')
+      .reply(200, function (uri, requestBody) {
+        assert.deepStrictEqual(requestBody.args, [
+          'html',
+          {}
+        ]);
+
+        return {
+          value: {}
+        };
+      });
+    
+    let axeResult;
+    await this.client.api.a11y.runTests(function (result) {
+      axeResult = result;
     });
+    await this.client.start();
+
+    assert.deepStrictEqual(axeResult, {});
   });
 });
