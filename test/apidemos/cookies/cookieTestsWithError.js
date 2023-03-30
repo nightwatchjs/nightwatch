84cb5d7ed3ebbbae0e066b68d5f3d0cb3973ce61
Priyansh Garg <39924567+garg3133@users.noreply.github.com>
2023-03-30 13:37:18 +0530

    Move cookie-related commands to `.cookies` ns. (#3610)

--- test/apidemos/cookies/cookieTestsWithError.js
+++ test/apidemos/cookies/cookieTestsWithError.js
@@ -17,4 +17,16 @@ describe('Cookie api demo tests', function() {
     assert.strictEqual(cookies, null);
   });
 
+  test('browser.cookies.getAll() with network errors', async (browser) => {
+    const cookies = await browser.cookies.getAll(res => {
+      assert.ok(res.error instanceof Error);
+      assert.strictEqual(res.error.code, 'ECONNRESET');
+      assert.strictEqual(res.error.message, 'ECONNRESET socket hang up');
+      assert.strictEqual(res.status, -1);
+      assert.strictEqual(res.value, null);
+    });
+
+    assert.strictEqual(cookies, null);
+  });
+
 });
