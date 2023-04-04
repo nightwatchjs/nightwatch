60a031bb5e02dcd811d545a0f256b72c89b4a277
Binayak Ghosh <ghoshbinayak@gmail.com>
2023-04-05 01:14:29 +0530

    fix nightwatchjs/nightwatch#3571; return correct status code for waitForElementNotPresent

--- test/src/element/testCommandsElementSelectors.js
+++ test/src/element/testCommandsElementSelectors.js
@@ -199,11 +199,11 @@ describe('test commands element selectors', function() {
 
     Nightwatch.api()
       .waitForElementNotPresent('.nock-string', 50, 20, false, function callback(result) {
-        assert.strictEqual(result.status, 0, 'waitForElementNotPresent "succeeds"');
+        assert.strictEqual(result.status, -1, 'waitForElementNotPresent "succeeds"');
         assert.strictEqual(result.value.length, 1, 'waitForElementNotPresent returns the found elements');
       })
       .waitForElementNotPresent({selector: '.nock-object', timeout: 50, retryInterval: 20, abortOnFailure: false}, function callback(result) {
-        assert.strictEqual(result.status, 0, 'waitForElementNotPresent "succeeds"');
+        assert.strictEqual(result.status, -1, 'waitForElementNotPresent "succeeds"');
         assert.strictEqual(result.value.length, 1, 'waitForElementNotPresent returns the found elements');
       });
 
