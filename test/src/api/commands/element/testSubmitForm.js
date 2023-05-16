de615aa94b72b25f97cd3261ffb955b58536554c
Binayak Ghosh <ghoshbinayak@gmail.com>
2023-05-16 06:36:47 -0700

    fix #3571: return correct status code for waitForElementNotPresent (#3678)
    * fix nightwatchjs/nightwatch#3571; return correct status code for waitForElementNotPresent
    
    * 
    ---------
    
    Co-authored-by: Harshit Agrawal <harshit.a@browserstack.com>
    Co-authored-by: Harshit Agrawal <94462364+harshit-bs@users.noreply.github.com>
    Co-authored-by: Binayak Ghosh <ghoshbinayak@gmail.com>
    
    * update tests
    
    * Revert "Feat/rerun (#3703)"
    
    This reverts commit 020601e08b5875509018f3777ba840705b50c454.
    
    * revert and skip hasAttribute test
    
    * refactor tests
    
    ---------
    
    Co-authored-by: Prudhvi <prudhvi@browserstack.com>
    Co-authored-by: Harshit Agrawal <harshit.a@browserstack.com>
    Co-authored-by: Harshit Agrawal <94462364+harshit-bs@users.noreply.github.com>

--- test/src/api/commands/element/testSubmitForm.js
+++ test/src/api/commands/element/testSubmitForm.js
@@ -11,14 +11,14 @@ describe('submitForm', function () {
     CommandGlobals.afterEach.call(this, done);
   });
 
+
   it('client.submitForm()', function (done) {
     MockServer.addMock({
       url: '/wd/hub/session/1352110219202/execute/sync',
       method: 'POST',
       response: JSON.stringify({
-        sessionId: '1352110219202',
-        status: 0
-      })
+        value: null
+      }, true, true)
     });
 
     this.client.api.submitForm('#weblogin', function callback(result) {
