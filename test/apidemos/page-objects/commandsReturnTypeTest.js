3fa9224710a360a49904a95a4645c86625991858
Priyansh Garg <priyanshgarg30@gmail.com>
2023-07-03 19:31:05 +0530

    Add tests.

---
+++ test/apidemos/page-objects/commandsReturnTypeTest.js
@@ -0,0 +1,102 @@
+const assert = require('assert');
+
+describe('test return type of various commands on page-objects', function () {
+  after(browser => browser.end());
+
+  const pageObject = this.page.simplePageObj();
+
+  it('return correct type on page objects in non-async mode', function() {
+    const pageClick = pageObject.click('@loginCss');
+    // pageClick does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageClick.submit, 'undefined');
+    assert.strictEqual(typeof pageClick.isChrome, 'undefined');
+    // pageClick has page specific methods/properties
+    assert.strictEqual(typeof pageClick.api, 'object');
+    assert.strictEqual(typeof pageClick.testCommand, 'function');
+
+    const pageAssert = pageObject.assert.visible('@loginCss');
+    // pageAssert does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageAssert.submit, 'undefined');
+    assert.strictEqual(typeof pageAssert.isChrome, 'undefined');
+    // pageAssert has page specific methods/properties
+    assert.strictEqual(typeof pageAssert.api, 'object');
+    assert.strictEqual(typeof pageAssert.testCommand, 'function');
+
+    const pageNodeAssert = pageObject.assert.equal(1, 1);
+    // pageNodeAssert does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageNodeAssert.submit, 'undefined');
+    assert.strictEqual(typeof pageNodeAssert.isChrome, 'undefined');
+    // pageNodeAssert has page specific methods/properties
+    assert.strictEqual(typeof pageNodeAssert.api, 'object');
+    assert.strictEqual(typeof pageNodeAssert.testCommand, 'function');
+
+    const pageCustomCommand = pageObject.testCommand();
+    // pageCustomCommand does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageCustomCommand.submit, 'undefined');
+    assert.strictEqual(typeof pageCustomCommand.isChrome, 'undefined');
+    // pageCustomCommand has page specific methods/properties
+    assert.strictEqual(typeof pageCustomCommand.api, 'object');
+    assert.strictEqual(typeof pageCustomCommand.testCommand, 'function');
+
+    const pageChaiAssert = pageObject.expect.element('@loginCss').to.be.visible;
+    // pageChaiAssert does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageChaiAssert.submit, 'undefined');
+    assert.strictEqual(typeof pageChaiAssert.isChrome, 'undefined');
+    // pageChaiAssert *does not* have page specific methods/properties
+    assert.strictEqual(typeof pageChaiAssert.api, 'undefined');
+    assert.strictEqual(typeof pageChaiAssert.testCommand, 'undefined');
+    // pageChaiAssert only have page Expect property/methods
+    assert.strictEqual(typeof pageChaiAssert.a, 'function');
+    assert.strictEqual(typeof pageChaiAssert.present, 'object');
+  });
+
+  it('return correct type on page objects in async mode', async function() {
+    const pageClick = pageObject.click('@loginCss');
+    assert.strictEqual(pageClick instanceof Promise, true);
+    // pageClick does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageClick.submit, 'undefined');
+    assert.strictEqual(typeof pageClick.isChrome, 'undefined');
+    // pageClick has page specific methods/properties
+    assert.strictEqual(typeof pageClick.api, 'object');
+    assert.strictEqual(typeof pageClick.testCommand, 'function');
+
+    const pageAssert = pageObject.assert.visible('@loginCss');
+    assert.strictEqual(pageAssert instanceof Promise, true);
+    // pageAssert does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageAssert.submit, 'undefined');
+    assert.strictEqual(typeof pageAssert.isChrome, 'undefined');
+    // pageAssert has page specific methods/properties
+    assert.strictEqual(typeof pageAssert.api, 'object');
+    assert.strictEqual(typeof pageAssert.testCommand, 'function');
+
+    const pageNodeAssert = pageObject.assert.equal(1, 1);
+    assert.strictEqual(pageNodeAssert instanceof Promise, true);
+    // pageNodeAssert does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageNodeAssert.submit, 'undefined');
+    assert.strictEqual(typeof pageNodeAssert.isChrome, 'undefined');
+    // pageNodeAssert has page specific methods/properties
+    assert.strictEqual(typeof pageNodeAssert.api, 'object');
+    assert.strictEqual(typeof pageNodeAssert.testCommand, 'function');
+
+    const pageCustomCommand = pageObject.testCommand();
+    assert.strictEqual(pageCustomCommand instanceof Promise, false);
+    // pageCustomCommand does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageCustomCommand.submit, 'undefined');
+    assert.strictEqual(typeof pageCustomCommand.isChrome, 'undefined');
+    // pageCustomCommand has page specific methods/properties
+    assert.strictEqual(typeof pageCustomCommand.api, 'object');
+    assert.strictEqual(typeof pageCustomCommand.testCommand, 'function');
+
+    const pageChaiAssert = pageObject.expect.element('@loginCss').to.be.visible;
+    assert.strictEqual(pageChaiAssert instanceof Promise, true);
+    // pageChaiAssert does not have methods specific to NightwatchAPI
+    assert.strictEqual(typeof pageChaiAssert.submit, 'undefined');
+    assert.strictEqual(typeof pageChaiAssert.isChrome, 'undefined');
+    // pageChaiAssert *does not* have page specific methods/properties
+    assert.strictEqual(typeof pageChaiAssert.api, 'undefined');
+    assert.strictEqual(typeof pageChaiAssert.testCommand, 'undefined');
+    // pageChaiAssert only have page Expect property/methods
+    assert.strictEqual(typeof pageChaiAssert.a, 'function');
+    assert.strictEqual(typeof pageChaiAssert.present, 'object');
+  });
+});
---
+++ test/src/apidemos/page-objects/testCommandsReturnType.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const MockServer = require('../../../lib/mockserver.js');
+const common = require('../../../common.js');
+const {settings} = common;
+const NightwatchClient = common.require('index.js');
+
+describe('new element api with page objects', function() {
+  beforeEach(function(done) {
+    this.server = MockServer.init();
+    this.server.on('listening', () => {
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    this.server.close(function() {
+      done();
+    });
+  });
+
+  it('test custom command', function() {
+    const testsPath = path.join(__dirname, '../../../apidemos/page-objects/commandsReturnTypeTest.js');
+
+    MockServer
+      .addMock({
+        url: '/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/click',
+        method: 'POST',
+        response: JSON.stringify({
+          value: null
+        }),
+        times: 2
+      })
+      .addMock({
+        url: '/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/clear',
+        method: 'POST',
+        response: JSON.stringify({
+          value: null
+        }),
+        times: 2
+      })
+      .addMock({
+        url: '/session/13521-10219-202/execute/sync',
+        method: 'POST',
+        response: JSON.stringify({
+          value: true
+        }),
+        times: 4  
+      });
+
+    const globals = {
+      calls: 0,
+      waitForConditionPollInterval: 50,
+      waitForConditionTimeout: 120,
+      retryAssertionTimeout: 100,
+
+      reporter(results) {
+        if (results.lastError) {
+          throw results.lastError;
+        }
+      }
+    };
+
+    return NightwatchClient.runTests(testsPath, settings({
+      selenium: {
+        port: null,
+        start_process: false
+      },
+      selenium_host: null,
+      webdriver: {
+        port: 10195,
+        start_process: false
+      },
+      output: false,
+      skip_testcases_on_fail: false,
+      page_objects_path: [path.join(__dirname, '../../../extra/pageobjects/pages')],
+      globals
+    }));
+  });
+});
