c0be5a8999c5810af42bfbc2ec610d48c0ce05ab
Kiss Róbert <ert78gb@gmail.com>
2023-08-04 22:57:38 +0200

    fix: es6 module import

---
+++ test/src/utils/testRequireModule.js
@@ -0,0 +1,20 @@
+const {strict: assert} = require('node:assert');
+const path = require('node:path');
+
+const requireModule = require('../../../dist/utils/requireModule.js');
+
+describe('test requireModule', function () {
+  it('should load commonjs file', function () {
+    const modulePath = path.join(__dirname, './__data__/meaning-of-life.js');
+    const meaningOfLife = requireModule(modulePath);
+
+    assert.equal(meaningOfLife, 42);
+  });
+
+  it('should load es6 module', async function () {
+    const modulePath = path.join(__dirname, './__data__/meaning-of-life.mjs');
+    const meaningOfLife = await requireModule(modulePath);
+
+    assert.equal(meaningOfLife, 42);
+  });
+});
