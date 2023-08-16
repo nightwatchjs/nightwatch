9dbe236f1ffbd9cdef63678c1458c2890463fe71
yash <yashsingh.here07@gmail.com>
2023-08-16 21:27:31 +0530

    rectified types in seleniumAtoms.js file

--- lib/utils/seleniumAtoms.ts
+++ lib/utils/seleniumAtoms.ts
@@ -1,24 +1,21 @@
+import path from 'path';
+
 const getAttribute = 'get-attribute.js';
 const isDisplayed = 'is-displayed.js';
 const findElements = 'find-elements.js';
 
-const path = require('path');
 const SELENIUM_ATOMS_PATH = path.join(path.dirname(require.resolve('selenium-webdriver')), 'lib', 'atoms');
 
-/**
- * @param {string} module
- * @return {!Function}
- */
-function requireAtom(module: string): Function {
+function requireAtom(module: string): unknown {
   try {
     return require(path.join(SELENIUM_ATOMS_PATH, module));
-  } catch (ex: any) {
+  } catch (ex) {
     throw Error(`Failed to import atoms module ${module} using Selenium path: ${SELENIUM_ATOMS_PATH}`);
   }
 }
 
-module.exports = {
-  requireIsDisplayed(): Function {
+export = {
+  requireIsDisplayed() {
     return requireAtom(isDisplayed);
   }
 };
