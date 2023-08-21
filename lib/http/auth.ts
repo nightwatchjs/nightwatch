a543832ff6f713a09612bb474d55ac7a12703699
yash <yashsingh.here07@gmail.com>
2023-08-21 15:14:37 +0530

    migrate lib/http/auth.js file to ts

---
+++ lib/http/auth.ts
@@ -0,0 +1,27 @@
+const HttpUtil = require('./http.js');
+import { ClientRequest } from "http";
+
+
+class Auth {
+  #httpRequest: ClientRequest;
+
+  constructor(httpRequest: ClientRequest) {
+    this.#httpRequest = httpRequest;
+  }
+
+  basic(user:string, pass:string): string {
+    return `Basic ${Auth.toBase64(`${user}:${pass}`)}`;
+  }
+
+  addAuth(user: string, pass:string): void {
+    if (user && pass) {
+      this.#httpRequest.setHeader(HttpUtil.Headers.AUTHORIZATION, this.basic(user, pass));
+    }
+  }
+
+  static toBase64(str: string): string {
+    return Buffer.from(str).toString('base64');
+  }
+}
+
+module.exports = Auth;
