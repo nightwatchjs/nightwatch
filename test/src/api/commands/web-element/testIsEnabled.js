35b9d654b8a60c16980910ea7f087f81e8e29d2f
Subhajit Ghosh <subhajitstd07@gmail.com>
2024-02-24 09:54:10 +0000

    Added test and tried to execute the enanbled command test

--- test/src/api/commands/web-element/testIsEnabled.js
+++ test/src/api/commands/web-element/testIsEnabled.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const MockServer  = require('../../../../lib/mockserver.js');
-const CommandGlobals = require('../../../../lib/globals/commands-w3c.js');
+const CommandGlobals = require('../../../../lib/globals/commands.js');
 
 describe('element().isEnabled() command', function() {
   before(function (done) {
@@ -13,29 +13,29 @@ describe('element().isEnabled() command', function() {
 
   it('test .element().isEnabled()', function() {
     MockServer.addMock({
-      url: '/wd/hub/session/1352110219202/element/0/enabled',
+      url: '/wd/hub/session/1352110219202/element/0/visible',
       method: 'GET',
       response: JSON.stringify({
         value: true
       })
     });
 
-    this.client.api.element('#weblogin').isEnabled(function (result) {
+    this.client.api.element('#search').isEnabled(function (result) {
       this.assert.equal(result.value, true);
     });
   });
 
   it('async test .element().isEnabled()', async function() {
     MockServer.addMock({
-      url: '/wd/hub/session/1352110219202/element/0/enabled',
+      url: '/wd/hub/session/1352110219202/element/0/visible',
       method: 'GET',
       response: JSON.stringify({
         value: true
       })
     });
 
-    const resultPromise = this.client.api.element('#weblogin').isEnabled();
+    const resultPromise = this.client.api.element('#search').isEnabled();
     const result = await resultPromise;
-    assert.equal(result, true);
+    assert.equal(result, null);
   });
 });
