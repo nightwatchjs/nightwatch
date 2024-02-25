585d21e81fb5bbeeb9d158936729d0db0e7bbe63
Aniket Singh Rawat <sprx@gmail.com>
2024-02-25 21:52:56 +0530

    updated tests

--- test/src/api/commands/web-element/testIsSelected.js
+++ test/src/api/commands/web-element/testIsSelected.js
@@ -1,46 +1,42 @@
 const assert = require('assert');
 const MockServer  = require('../../../../lib/mockserver.js');
-const CommandGlobals = require('../../../../lib/globals/commands.js');
+const CommandGlobals = require('../../../../lib/globals/commands-w3c.js');
 
 describe('element().isSelected() command', function() {
   before(function (done) {
     CommandGlobals.beforeEach.call(this, done);
   });
 
-  after(function(done) {
+  after(function (done) {
     CommandGlobals.afterEach.call(this, done);
   });
 
-  it('test .element().isSelected()', function() {
+  it('test .element().isSelected() selected', async function() {
     MockServer.addMock({
-      url: '/wd/hub/session/1352110219202/element/0/selected',
+      url: '/session/13521-10219-202/element/0/selected',
       method: 'GET',
       response: JSON.stringify({
-        sessionId: '1352110219202',
-        status: 0,
         value: true
       })
-    });
+    }, true);
 
-    this.client.api.element('#weblogin').isSelected(function (result) {
-      this.assert.equal(result.value, true);
-    });
+    const resultPromise = this.client.api.element('#signupSection').isSelected();
+    const result = await resultPromise;
+    assert.strictEqual(result, true);
   });
 
-  it('async test .element().isSelected()', async function() {
+  it('test .element().isSelected() not selected', async function() {
     MockServer.addMock({
-      url: '/wd/hub/session/1352110219202/element/0/selected',
+      url: '/session/13521-10219-202/element/0/selected',
       method: 'GET',
       response: JSON.stringify({
-        sessionId: '1352110219202',
-        status: 0,
-        value: true
+        value: false
       })
-    });
+    }, true);
 
-    const resultPromise = this.client.api.element('#weblogin').isSelected();
+    const resultPromise = this.client.api.element('#signupSection').isSelected();
     const result = await resultPromise;
-    assert.equal(result, true);
+    assert.strictEqual(result, false);
   });
 });
 
