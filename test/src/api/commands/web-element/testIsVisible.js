b752715657cdd4c2e56b7e84a66e1531e9deee1c
Subhajit Ghosh <subhajitstd07@gmail.com>
2024-02-26 14:03:21 +0000

    Added successfull and failed test

--- test/src/api/commands/web-element/testIsVisible.js
+++ test/src/api/commands/web-element/testIsVisible.js
@@ -1,41 +1,62 @@
 const assert = require('assert');
+const {WebElement} = require('selenium-webdriver');
 const MockServer  = require('../../../../lib/mockserver.js');
-const CommandGlobals = require('../../../../lib/globals/commands.js');
-
+const CommandGlobals = require('../../../../lib/globals/commands-w3c.js');
+const common = require('../../../../common.js');
+const Element = common.require('element/index.js');
 describe('element().isVisible() command', function() {
   before(function (done) {
     CommandGlobals.beforeEach.call(this, done);
   });
 
-  after(function(done) {
+  after(function (done) {
     CommandGlobals.afterEach.call(this, done);
   });
 
-  it('test .element().isVisible()', function() {
+  it('test .element().isVisible() Visible', async function() {
     MockServer.addMock({
-      url: '/wd/hub/session/1352110219202/element/0/visible',
+      url: '/session/13521-10219-202/element/0/Visible',
       method: 'GET',
       response: JSON.stringify({
         value: true
       })
-    });
+    }, true);
 
     this.client.api.element('#search').isVisible(function (result) {
       this.assert.equal(result.value, true);
     });
   });
 
-  it('async test .element().isVisible()', async function() {
+  it('test .element().isVisible() not Visible', async function() {
     MockServer.addMock({
-      url: '/wd/hub/session/1352110219202/element/0/visible',
+      url: '/session/13521-10219-202/element/0/Visible',
       method: 'GET',
       response: JSON.stringify({
-        value: true
+        value: false
       })
-    });
+    }, true);
 
-    const resultPromise = this.client.api.element('#search').isVisible();
+    this.client.api.element('#search').isVisible(function (result) {
+      this.assert.equal(result.value, false);
+    });
+  });
+  it('test .element().find().isVisible() not visible', async function () {
+    MockServer.addMock({
+      url: '/session/13521-10219-202/element/0/displayed',
+      method: 'GET',
+      response: JSON.stringify({
+        value: false
+      })
+    }, true);
+
+    const resultPromise = this.client.api.element('#signupSection').find('#helpBtn').isVisible();
+    assert.strictEqual(resultPromise instanceof Element, false);
+    assert.strictEqual(typeof resultPromise.find, 'undefined');
+
+    assert.strictEqual(resultPromise instanceof Promise, false);
+    assert.strictEqual(typeof resultPromise.then, 'function');
+
     const result = await resultPromise;
-    assert.equal(result, true);
+    assert.strictEqual(result, null);
   });
 });
