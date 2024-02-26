6cbdecbf9b737c3b9ab07fbb4f51911fbf47e349
Madhav Saini <m_saini@ece.iitr.ac.in>
2024-02-26 15:30:22 +0530

    updated test case

--- test/src/api/commands/element/testClearValue.js
+++ test/src/api/commands/element/testClearValue.js
@@ -1,147 +1,181 @@
-const assert = require('assert');
-const MockServer  = require('../../../../lib/mockserver.js');
-const Nightwatch = require('../../../../lib/nightwatch.js');
-const {Key} = require('selenium-webdriver');
+const assert = require("assert");
+const MockServer = require("../../../../lib/mockserver.js");
+const Nightwatch = require("../../../../lib/nightwatch.js");
+const { Key } = require("selenium-webdriver");
 
-describe('clearValue', function() {
-
-  before(function(done) {
+describe("clearValue", function () {
+  before(function (done) {
     this.server = MockServer.init();
-    this.server.on('listening', () => done());
+    this.server.on("listening", () => done());
   });
 
-  after(function(done) {
+  after(function (done) {
     this.server.close(() => done());
   });
 
-  it('client.clearValue()', function(done) {
+  it("client.clearValue()", function (done) {
     Nightwatch.initClient({
       output: false,
-      silent: false
-    })
-      .then(client => {
-        MockServer.addMock({
-          'url': '/wd/hub/session/1352110219202/element/0/clear',
-          'response': {
-            sessionId: '1352110219202',
-            status: 0
-          }
-        });
+      silent: false,
+    }).then((client) => {
+      MockServer.addMock({
+        url: "/wd/hub/session/1352110219202/element/0/clear",
+        response: {
+          sessionId: "1352110219202",
+          status: 0,
+        },
+      });
 
-        client.api.clearValue('#weblogin', function callback(result) {
+      client.api
+        .clearValue("#weblogin", function callback(result) {
           assert.strictEqual(result.status, 0);
           assert.strictEqual(this, client.api);
         })
-          .clearValue('css selector', '#weblogin', function callback(result) {
-            assert.strictEqual(result.status, 0);
-          });
-
-        client.start(done);
-      });
-  });
-
-  it('client.clearValue() with invalid locate strategy', function(done) {
-    Nightwatch.initClient({
-      output: false,
-      silent: false
-    })
-      .then(client => {
-        client.api.clearValue('invalid strategy', '#weblogin', function callback(result) {
+        .clearValue("css selector", "#weblogin", function callback(result) {
           assert.strictEqual(result.status, 0);
         });
 
-        client.start(function(err) {
-          try {
-            assert.ok(err instanceof Error);
-            assert.ok(err.message.includes('Provided locating strategy "invalid strategy" is not supported for .clearValue()'));
-            done();
-          } catch (e) {
-            done(e);
-          }
-        });
-      });
+      client.start(done);
+    });
   });
 
-  it('client.clearValue() with no callback', function(done) {
+  it("client.clearValue() with invalid locate strategy", function (done) {
     Nightwatch.initClient({
-      output: false,
-      silent: false
-    })
-      .then(client => {
-        MockServer.addMock({
-          'url': '/wd/hub/session/1352110219202/element/0/clear',
-          'response': {
-            sessionId: '1352110219202',
-            status: 0
-          }
-        });
-
-
-        client.api.elements('css selector', 'body', res => {
-          client.api.clearValue('#weblogin');
-        });
-
-        client.start(done);
-      });
-  });
-
-  it('client.clearValue() with webdriver protocol', function(done) {
-    Nightwatch.initClient({
-      selenium: {
-        version2: false,
-        start_process: false,
-        host: null
-      },
       output: false,
       silent: false,
-      webdriver: {
-        host: 'localhost',
-        start_process: false
-      }
-    }).then(client => {
-      MockServer.addMock({
-        url: '/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/clear',
-        response: {value: null}
-      }, true);
+    }).then((client) => {
+      client.api.clearValue(
+        "invalid strategy",
+        "#weblogin",
+        function callback(result) {
+          assert.strictEqual(result.status, 0);
+        }
+      );
 
-      MockServer.addMock({
-        url: '/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/clear',
-        response: {value: null}
-      }, true);
+      client.start(function (err) {
+        try {
+          assert.ok(err instanceof Error);
+          assert.ok(
+            err.message.includes(
+              'Provided locating strategy "invalid strategy" is not supported for .clearValue()'
+            )
+          );
+          done();
+        } catch (e) {
+          done(e);
+        }
+      });
+    });
+  });
 
-      client.api.clearValue('#webdriver', function(result) {
-        assert.strictEqual(result.value, null);
-      }).clearValue('css selector', '#webdriver', function(result) {
-        assert.strictEqual(result.value, null);
+  it("client.clearValue() with no callback", function (done) {
+    Nightwatch.initClient({
+      output: false,
+      silent: false,
+    }).then((client) => {
+      MockServer.addMock({
+        url: "/wd/hub/session/1352110219202/element/0/clear",
+        response: {
+          sessionId: "1352110219202",
+          status: 0,
+        },
+      });
+
+      client.api.elements("css selector", "body", (res) => {
+        client.api.clearValue("#weblogin");
       });
 
       client.start(done);
     });
   });
- 
-  it('client.clearValue() with webdriver protocol - ensuring that fallback is working', function (done) {
+
+  it("client.clearValue() with webdriver protocol", function (done) {
     Nightwatch.initClient({
       selenium: {
         version2: false,
         start_process: false,
-        host: null
+        host: null,
+      },
+      output: false,
+      silent: false,
+      webdriver: {
+        host: "localhost",
+        start_process: false,
+      },
+    }).then((client) => {
+      MockServer.addMock(
+        {
+          url: "/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/clear",
+          response: { value: null },
+        },
+        true
+      );
+
+      MockServer.addMock(
+        {
+          url: "/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/clear",
+          response: { value: null },
+        },
+        true
+      );
+
+      client.api
+        .clearValue("#webdriver", function (result) {
+          assert.strictEqual(result.value, null);
+        })
+        .clearValue("css selector", "#webdriver", function (result) {
+          assert.strictEqual(result.value, null);
+        });
+
+      client.start(done);
+    });
+  });
+
+  it("client.clearValue() with webdriver protocol - ensuring that fallback is working", function (done) {
+    Nightwatch.initClient({
+      selenium: {
+        version2: false,
+        start_process: false,
+        host: null,
       },
       output: true,
       silent: false,
       webdriver: {
-        host: 'localhost',
-        start_process: false
-      }
+        host: "localhost",
+        start_process: false,
+      },
     }).then((client) => {
-      MockServer.addMock({
-        url: '/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/property/clear',
-        response: {value: null}
-      }, true);
-      
+      const bArr = Array(6).fill(Key.BACK_SPACE);
+      const str = bArr.join("");
 
-      
 
-      client.api.clearValue('#webdriver', function (result) {
+      MockServer.addMock(
+        {
+          url: "/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/clear",
+          response: { value: 'sample' },
+        },
+        true
+      );
+
+     
+      MockServer.addMock(
+        {
+          url: `/session/13521-10219-202/element/5cc459b8-36a8-3042-8b4a-258883ea642b/value`,
+          method: "POST",
+          postdata: {
+            str,
+            value: bArr,
+          },
+          response: {
+            value: null,
+          },
+          statusCode: 200,
+        },
+        true
+      );
+     
+
+      client.api.clearValue("#webdriver", function (result) {
         assert.strictEqual(result.value, null);
         // ensures fallback is working
       });
@@ -150,47 +184,57 @@ describe('clearValue', function() {
     });
   });
 
-  it('client.clearValue() with webdriver protocol - element not found', function(done) {
+  it("client.clearValue() with webdriver protocol - element not found", function (done) {
     Nightwatch.initClient({
       selenium: {
         version2: false,
         start_process: false,
-        host: null
+        host: null,
       },
       webdriver: {
-        host: 'localhost',
-        start_process: false
+        host: "localhost",
+        start_process: false,
       },
       silent: false,
       output: false,
       globals: {
-        waitForConditionPollInterval: 5
-      }
-    }).then(client => {
+        waitForConditionPollInterval: 5,
+      },
+    }).then((client) => {
       let retries = 0;
       MockServer.addMock({
         statusCode: 404,
-        url: '/session/13521-10219-202/elements',
-        postdata: {using: 'css selector', value: '#webdriver-notfound'},
+        url: "/session/13521-10219-202/elements",
+        postdata: { using: "css selector", value: "#webdriver-notfound" },
         times: 2,
         onResponse() {
           retries++;
         },
         response: {
           value: {
-            error: 'no such element',
-            message: 'Unable to locate element: #webdriver-notfound',
-            stacktrace: 'WebDriverError@chrome://marionette/content/error.js:172:5\nNoSuchElementError@chrome://marionette/content/error.js:400:5'
-          }
-        }
+            error: "no such element",
+            message: "Unable to locate element: #webdriver-notfound",
+            stacktrace:
+              "WebDriverError@chrome://marionette/content/error.js:172:5\nNoSuchElementError@chrome://marionette/content/error.js:400:5",
+          },
+        },
       });
 
-      client.api.clearValue({selector: '#webdriver-notfound', timeout: 11}, function(result) {
-        assert.strictEqual(result.status, -1);
-        assert.strictEqual(retries, 2);
-        assert.strictEqual(result.value.error, 'An error occurred while running .clearValue() command on <#webdriver-notfound>: Timed out while waiting for element "#webdriver-notfound" with "css selector" to be present for 11 milliseconds.');
-        assert.strictEqual(result.value.message, 'An error occurred while running .clearValue() command on <#webdriver-notfound>: Timed out while waiting for element "#webdriver-notfound" with "css selector" to be present for 11 milliseconds.');
-      });
+      client.api.clearValue(
+        { selector: "#webdriver-notfound", timeout: 11 },
+        function (result) {
+          assert.strictEqual(result.status, -1);
+          assert.strictEqual(retries, 2);
+          assert.strictEqual(
+            result.value.error,
+            'An error occurred while running .clearValue() command on <#webdriver-notfound>: Timed out while waiting for element "#webdriver-notfound" with "css selector" to be present for 11 milliseconds.'
+          );
+          assert.strictEqual(
+            result.value.message,
+            'An error occurred while running .clearValue() command on <#webdriver-notfound>: Timed out while waiting for element "#webdriver-notfound" with "css selector" to be present for 11 milliseconds.'
+          );
+        }
+      );
 
       client.start(done);
     });
