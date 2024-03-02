2add8106a6ec16540bd8a66f0b165bd25c3d68b2
Aniket Singh Rawat <sprx077@gmail.com>
2024-03-02 15:06:19 +0530

    added test for checking redacted values in reporter

--- test/src/api/commands/element/testSetPassword.js
+++ test/src/api/commands/element/testSetPassword.js
@@ -1,5 +1,9 @@
+const path = require('path');
 const assert = require('assert');
 const {Key} = require('selenium-webdriver');
+const common = require('../../../../common.js');
+const {settings} = common;
+const NightwatchClient = common.require('index.js');
 const MockServer  = require('../../../../lib/mockserver.js');
 const CommandGlobals = require('../../../../lib/globals/commands.js');
 
@@ -57,5 +61,40 @@ describe('setPassword', function() {
       });
 
     this.client.start(done);
+  });
+
+  it('client.setPassword() value hidden', async function() {
+    MockServer.addMock({
+      url: '/wd/hub/session/1352110219202/element/0/value',
+      method: 'POST',
+      postdata: {
+        text: Key.NULL + 'password',
+        value: [Key.NULL, 'p', 'a', 's', 's', 'w', 'o', 'r', 'd']
+      },
+      response: {
+        sessionId: '1352110219202',
+        status: 0
+      }
+    });
+
+    const testsPath = [
+      path.join(__dirname, '../../../../sampletests/checkValueRedacted/passwordValueRedacted.js')
+    ];
+
+    const globals = {
+      calls: 0,
+      waitForConditionTimeout: 10,
+      waitForConditionPollInterval: 10,
+      reporter(results) {
+        assert.strictEqual(globals.calls, 2);
+        const rawHttpOutput = results.modules.passwordValueRedacted.rawHttpOutput;
+        assert.strictEqual(rawHttpOutput[24][1], '  Request POST /wd/hub/session/1352110219202/element/0/value  ');
+        assert.strictEqual(rawHttpOutput[24][2], '{ text: \'****\', value: [ \'*\', \'*\', \'*\', \'*\' ] }');
+      }
+    };
+
+    await NightwatchClient.runTests(testsPath, settings({
+      globals
+    }));
   });
 });
