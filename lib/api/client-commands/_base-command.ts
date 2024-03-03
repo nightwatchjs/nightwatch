3b44e7067342d3263513316d4f7b981e5ccb83f4
Honey2339 <prasoon2study@gmail.com>
2024-03-03 12:22:35 +0530

    Migrating lib/api/client-commands/_base-command.js to TS

---
+++ lib/api/client-commands/_base-command.ts
@@ -0,0 +1,102 @@
+interface Result {
+    status: number;
+    code?: string;
+    name?: string;
+    value: any;
+    error?: string;
+    message? : string;
+}
+  
+type PerformActionFunction = (callback: (result: Result | Error) => void) => void;
+type UserSuppliedCallbackFunction = (resultValue: any) => Promise<any> | void;
+  
+class ClientCommand {
+
+    static get isTraceable(): boolean {
+        return false;
+    }
+
+    transportActions : any;
+
+    static makePromise({
+        performAction,
+        userSuppliedCallback = () => {},
+        fullResultObject = true,
+        fullPromiseResolve = true
+    }: {
+        performAction: PerformActionFunction;
+        userSuppliedCallback?: UserSuppliedCallbackFunction;
+        fullResultObject?: boolean;
+        fullPromiseResolve?: boolean;
+    }): Promise<any> {
+        return new Promise((resolve, reject) => {
+        performAction((result : Result | Error) => {
+            try {
+            if (result) {
+                const { name, message } = result;
+                const code = (result as Result).code;
+
+                result = {
+                status: -1,
+                code,
+                name,
+                value: {
+                    message
+                },
+                error: message
+                };
+            }
+
+            const resultValue = fullResultObject ? result : result.value;
+            let promise = userSuppliedCallback.call(this, resultValue);
+            if (!(promise instanceof Promise)) {
+                promise = Promise.resolve(promise);
+            }
+
+            const resolveValue = fullPromiseResolve ? result : result.value;
+            promise.then(_ => resolve(resolveValue)).catch(err => reject(err));
+            } catch (e) {
+            reject(e);
+            }
+        });
+        });
+    }
+
+    returnsFullResultObject(): boolean {
+        return true;
+    }
+
+    resolvesWithFullResultObject(): boolean {
+        return true;
+    }
+
+    reportProtocolErrors(result): boolean {
+        return true;
+    }
+
+    command(performAction : PerformActionFunction,userSuppliedCallback: UserSuppliedCallbackFunction): Promise<any> {
+        return ClientCommand.makePromise({
+        performAction,
+        userSuppliedCallback,
+        fullResultObject: this.returnsFullResultObject(),
+        fullPromiseResolve: this.resolvesWithFullResultObject()
+        });
+    }
+
+    executeScriptHandler(
+        method: string,
+        script: string | Function,
+        args: any[],
+        callback: (result: any) => void
+    ): any {
+        let fn;
+
+        if (script instanceof Function) {
+        fn = `var passedArgs = Array.prototype.slice.call(arguments,0); return (${script.toString()}).apply(window, passedArgs);`;
+        } else {
+        fn = script;
+        }
+
+        return this.transportActions[method](fn, args, callback);
+    }
+}
