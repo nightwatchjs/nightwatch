55d1fb410bb3b0fae2de069266ca65734e1f8fe7
Aritra Banerjee <103491935+AritraLeo@users.noreply.github.com>
2024-03-08 22:29:48 +0530

    alias for isVisible with test cases passing

--- types/web-element.d.ts
+++ types/web-element.d.ts
@@ -194,6 +194,7 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
   isEnabled(): ElementValue<boolean>;
 
   isVisible(): ElementValue<boolean>;
+  isDisplayed(): ElementValue<boolean>;
 }
 
 type WaitUntilOptions = {
