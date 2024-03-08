02dd5f27ba2304c3eedb007a1ffcbd6996e4c390
Priyanshu <priyanshubutola@gmail.com>
2024-03-08 22:26:13 +0530

    Fix #4105: isDisplayed() alias added with types and tests

--- types/web-element.d.ts
+++ types/web-element.d.ts
@@ -2,11 +2,11 @@ import {
   By,
   RelativeBy,
   WebElement,
-  WebElementPromise
-} from 'selenium-webdriver';
+  WebElementPromise,
+} from "selenium-webdriver";
 
-import {ElementProperties} from './page-object';
-import {Element, LocateStrategy, NightwatchClient} from './index';
+import { ElementProperties } from "./page-object";
+import { Element, LocateStrategy, NightwatchClient } from "./index";
 
 export interface ScopedElement extends Element, PromiseLike<WebElement> {
   assert: ElementAssertions;
@@ -18,14 +18,14 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
 
   findByText(
     text: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly exact?: boolean;
     }
   ): ScopedElement;
 
   findByRole(
-    role: 'heading',
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    role: "heading",
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly level?: number;
       readonly checked?: boolean;
       readonly current?: boolean | string;
@@ -37,7 +37,7 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
 
   findByRole(
     role: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly current?: boolean | string;
       readonly checked?: boolean;
       readonly pressed?: boolean;
@@ -45,41 +45,41 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
       readonly expanded?: boolean;
     }
   ): ScopedElement;
-  
+
   findByPlaceholderText(
     text: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly exact?: boolean;
     }
   ): ScopedElement;
-  
+
   findByLabelText(
     text: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly exact?: boolean;
     }
   ): ScopedElement;
-  
+
   findByAltText(
     text: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly exact?: boolean;
     }
   ): ScopedElement;
-  
+
   findAll(selector: ScopedSelector | Promise<ScopedSelector>): Elements;
   getAll(selector: ScopedSelector | Promise<ScopedSelector>): Elements;
 
   findAllByText(
     text: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly exact?: boolean;
     }
   ): Elements;
 
   findAllByRole(
-    role: 'heading',
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    role: "heading",
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly level?: number;
       readonly checked?: boolean;
       readonly current?: boolean | string;
@@ -91,7 +91,7 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
 
   findAllByRole(
     role: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly current?: boolean | string;
       readonly checked?: boolean;
       readonly pressed?: boolean;
@@ -102,7 +102,7 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
 
   getAllByRole(
     role: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly current?: boolean | string;
       readonly checked?: boolean;
       readonly pressed?: boolean;
@@ -113,14 +113,14 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
 
   findAllByPlaceholderText(
     text: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly exact?: boolean;
     }
   ): Elements;
 
   findAllByAltText(
     text: string,
-    options?: Omit<ScopedSelectorObject, 'selector'> & {
+    options?: Omit<ScopedSelectorObject, "selector"> & {
       readonly exact?: boolean;
     }
   ): Elements;
@@ -133,7 +133,7 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
 
   getPreviousElementSibling(): ScopedElement;
 
-  getShadowRoot(): Omit<ScopedElement, 'then'> & PromiseLike<ShadowRoot>;
+  getShadowRoot(): Omit<ScopedElement, "then"> & PromiseLike<ShadowRoot>;
 
   getId(): ElementValue<string>;
 
@@ -189,11 +189,16 @@ export interface ScopedElement extends Element, PromiseLike<WebElement> {
 
   isSelected(): ElementValue<boolean>;
 
-  waitUntil(signalOrOptions: WaitUntilActions | WaitUntilOptions, waitOptions?: WaitUntilOptions): Promise<WebElement>;
+  waitUntil(
+    signalOrOptions: WaitUntilActions | WaitUntilOptions,
+    waitOptions?: WaitUntilOptions
+  ): Promise<WebElement>;
 
   isEnabled(): ElementValue<boolean>;
 
   isVisible(): ElementValue<boolean>;
+
+  isDisplayed(): ElementValue<boolean>;
 }
 
 type WaitUntilOptions = {
@@ -205,7 +210,16 @@ type WaitUntilOptions = {
   abortOnFailure?: boolean;
 };
 
-type WaitUntilActions = 'selected' | 'visible' | 'disabled' | 'enabled' | 'not.selected' | 'not.visible' | 'not.enabled' | 'present' | 'not.present';
+type WaitUntilActions =
+  | "selected"
+  | "visible"
+  | "disabled"
+  | "enabled"
+  | "not.selected"
+  | "not.visible"
+  | "not.enabled"
+  | "present"
+  | "not.present";
 
 export class Elements implements PromiseLike<WebElement[]> {
   constructor(
@@ -254,7 +268,6 @@ export class ElementsAssertions {
   constructor(elements: Elements, options: ElementsAssertionsOptions);
 
   get not(): ElementsAssertions;
-
 }
 
 export type ElementAssertionsOptions = {
@@ -299,7 +312,7 @@ export class ElementValue<T> implements PromiseLike<T> {
 
 export type ScopedSelectorObject = Omit<
   ElementProperties,
-  'webElement' | 'webElementId' | 'selector'
+  "webElement" | "webElementId" | "selector"
 > & {
   readonly selector: string | By | RelativeBy;
 };
@@ -342,8 +355,17 @@ export type DragAndDropDestination = {
 export interface ElementFunction
   extends Pick<
     ScopedElement,
-    'find' | 'findByText' | 'findByRole' | 'findByPlaceholderText' | 'findByLabelText' | 'findByAltText' |
-    'findAll' | 'findAllByText' | 'findAllByRole' | 'findAllByPlaceholderText' | 'findAllByAltText'
+    | "find"
+    | "findByText"
+    | "findByRole"
+    | "findByPlaceholderText"
+    | "findByLabelText"
+    | "findByAltText"
+    | "findAll"
+    | "findAllByText"
+    | "findAllByRole"
+    | "findAllByPlaceholderText"
+    | "findAllByAltText"
   > {
   (selector: ScopedElementSelector): ScopedElement;
   (
