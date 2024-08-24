bfafc677de7f3fd6b429f607a95cf6089193f071
divine7022 <www.divinehome12@gmail.com>
2024-08-24 18:07:35 +0530

    first commite

--- api/index.js
+++ api/index.js
@@ -1,32 +1,40 @@
-const path = require('path');
+const path = require("path");
+const Utils = require("../util/utils.js");
 const exportedCommands = [
-  'element-commands/findElement.js',
-  'element-commands/findElements.js',
-  'element-commands/element.js',
-  'element-commands/elements.js',
-  'element-commands/elementIdElement.js',
-  'element-commands/elementIdElements.js',
-  'element-commands/waitForElementPresent.js',
-  'element-commands/waitForElementNotPresent.js',
-  'element-commands/waitForElementVisible.js',
-  'element-commands/waitForElementNotVisible.js',
-  'protocol/quit.js'
+  "element-commands/findElement.js",
+  "element-commands/findElements.js",
+  "element-commands/element.js",
+  "element-commands/elements.js",
+  "element-commands/elementIdElement.js",
+  "element-commands/elementIdElements.js",
+  "element-commands/waitForElementPresent.js",
+  "element-commands/waitForElementNotPresent.js",
+  "element-commands/waitForElementVisible.js",
+  "element-commands/waitForElementNotVisible.js",
+  "protocol/quit.js",
+  "element-commands/frame.js",
 ];
 
-const basePath = '../lib/api';
+const basePath = "../lib/api";
 const Commands = {};
 const props = exportedCommands.reduce((prev, fileName) => {
-  const commandName = fileName.substring(fileName.lastIndexOf('/') + 1).replace('.js', '');
+  const commandName = fileName
+    .substring(fileName.lastIndexOf("/") + 1)
+    .replace(".js", "");
   prev[commandName] = {
     configurable: true,
-    get: function() {
+    get: function () {
       return require(path.join(basePath, fileName));
-    }
+    },
   };
 
   return prev;
 }, {});
 
 Object.defineProperties(Commands, props);
+
+Utils.isPageObject = function (selector) {
+  return selector && typeof selector === "object" && selector.selector;
+};
 
 module.exports = Commands;
