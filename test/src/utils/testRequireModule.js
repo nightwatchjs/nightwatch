9db5c457e7a8a0a5aafbf01549358adb3b7640b2
Samriddhi-35 <samriddhisharma632@gmail.com>
2026-01-06 20:23:01 +0530

    add unit tests

--- test/src/utils/testRequireModule.js
+++ test/src/utils/testRequireModule.js
@@ -18,4 +18,24 @@ describe('test requireModule', function () {
 
     assert.equal(meaningOfLife, 42);
   });
+
+  it('should load named exports from .mjs file', async function () {
+    const modulePath = path.join(__dirname, './__data__/named-exports.mjs');
+    const module = await requireModule(modulePath);
+
+    assert.equal(typeof module.functionA, 'function');
+    assert.equal(typeof module.functionB, 'function');
+  });
+
+  it('should load default and named exports from .mjs file', async function () {
+    const modulePath = path.join(__dirname, '__data__/mixed-exports.mjs');
+    const module = await requireModule(modulePath);
+
+    assert.equal(module. defaultValue, 'default-value');
+    assert.equal(module.defaultFunction(), 'default function called');
+    assert.equal(module.namedExport, 'named value');
+    assert.equal(module.namedFunction(), 'named function called');
+  });
+
+
 });
