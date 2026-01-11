9ed02de1009dfd10884407ef2a2e68fffae3213b
Priyansh Garg <priyanshgarg30@gmail.com>
2026-01-12 01:13:42 +0530

    Add test for ts-node behaviour.

--- test/src/utils/testRequireModule.js
+++ test/src/utils/testRequireModule.js
@@ -19,6 +19,21 @@ describe('test requireModule', function () {
     assert.equal(meaningOfLife, 42);
   });
 
+  it('should load es6 module with ts-node activated', async function () {
+    const tsNode = require('ts-node').register({
+      project: path.resolve('test', './tsconfig.json')
+    });
+
+    const modulePath = path.join(__dirname, './__data__/meaning-of-life.mjs');
+    // delete from cache to force reloading
+    delete require.cache[require.resolve(modulePath)];
+
+    const meaningOfLife = await requireModule(modulePath);
+
+    assert.equal(meaningOfLife, 42);
+    tsNode.enabled(false);
+  });
+
   it('should load named exports from .mjs file', async function () {
     const modulePath = path.join(__dirname, './__data__/named-exports.mjs');
     const module = await requireModule(modulePath);
@@ -31,11 +46,9 @@ describe('test requireModule', function () {
     const modulePath = path.join(__dirname, '__data__/mixed-exports.mjs');
     const module = await requireModule(modulePath);
 
-    assert.equal(module. defaultValue, 'default-value');
+    assert.equal(module.defaultValue, 'default-value');
     assert.equal(module.defaultFunction(), 'default function called');
     assert.equal(module.namedExport, 'named value');
     assert.equal(module.namedFunction(), 'named function called');
   });
-
-
 });
