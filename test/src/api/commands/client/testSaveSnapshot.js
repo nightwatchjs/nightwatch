7e6db7c0917b68e8d709113fbbfc252598214fcc
Priyansh Garg <priyanshgarg30@gmail.com>
2026-01-12 02:41:06 +0530

    Fix snapshot typo in testSaveSnapshot.js

--- test/src/api/commands/client/testSaveSnapshot.js
+++ test/src/api/commands/client/testSaveSnapshot.js
@@ -23,22 +23,22 @@ describe('.saveSnapshot()', function() {
 
   it('browser.saveScreenshot()', function(done) {
     const pageHtml = '<!doctype html><html class="no-js" lang=""><head> <meta charset="utf-8"> <title></title> <meta name="description" content=""> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta property="og:title" content=""> <meta property="og:type" content=""> <meta property="og:url" content=""> <meta property="og:image" content=""> <link rel="manifest" href="site.webmanifest"> <link rel="apple-touch-icon" href="icon.png"> <link rel="stylesheet" href="css/normalize.css"> <link rel="stylesheet" href="css/main.css"> <meta name="theme-color" content="#fafafa"></head><body> <p>Hello world! This is HTML5 Boilerplate.</p><script src="js/vendor/modernizr-3.11.2.min.js"></script> <script src="js/plugins.js"></script> <script src="js/main.js"></script> <script>window.ga=function (){ga.q.push(arguments)}; ga.q=[]; ga.l=+new Date; ga(\'create\', \'UA-XXXXX-Y\', \'auto\'); ga(\'set\', \'anonymizeIp\', true); ga(\'set\', \'transport\', \'beacon\'); ga(\'send\', \'pageview\') </script> <script src="https://www.google-analytics.com/analytics.js" async></script></body></html>';
-    const pageHtmlWithoutScript = '<!DOCTYPE html><html class="no-js" lang=""><head> <meta charset="utf-8"> <title></title> <meta name="description" content=""> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta property="og:title" content=""> <meta property="og:type" content=""> <meta property="og:url" content=""> <meta property="og:image" content=""> <link rel="manifest" href="site.webmanifest"> <link rel="apple-touch-icon" href="icon.png"> <link rel="stylesheet" href="css/normalize.css"> <link rel="stylesheet" href="css/main.css"> <meta name="theme-color" content="#fafafa"></head><body> <p>Hello world! This is HTML5 Boilerplate.</p>    </body></html>'
+    const pageHtmlWithoutScript = '<!DOCTYPE html><html class="no-js" lang=""><head> <meta charset="utf-8"> <title></title> <meta name="description" content=""> <meta name="viewport" content="width=device-width, initial-scale=1"> <meta property="og:title" content=""> <meta property="og:type" content=""> <meta property="og:url" content=""> <meta property="og:image" content=""> <link rel="manifest" href="site.webmanifest"> <link rel="apple-touch-icon" href="icon.png"> <link rel="stylesheet" href="css/normalize.css"> <link rel="stylesheet" href="css/main.css"> <meta name="theme-color" content="#fafafa"></head><body> <p>Hello world! This is HTML5 Boilerplate.</p>    </body></html>';
     const pageUrl = 'https://nightwatchjs.org';
 
     Nocks.getUrl(pageUrl);
     Nocks.getPageSource(pageHtml);
 
     Utils.writeToFile = async function(fileName, data) {
-      assert.strictEqual(fileName, 'snpashot.html');
+      assert.strictEqual(fileName, 'snapshot.html');
       assert.strictEqual(data, pageHtmlWithoutScript);
 
       return fileName;
     };
 
-    this.client.api.saveSnapshot('snpashot.html', function(result) {
+    this.client.api.saveSnapshot('snapshot.html', function(result) {
       assert.ok(result);
-      assert.deepStrictEqual(result, {snapshotFilePath: 'snpashot.html',
+      assert.deepStrictEqual(result, {snapshotFilePath: 'snapshot.html',
         snapshotUrl: pageUrl});
     });
 
